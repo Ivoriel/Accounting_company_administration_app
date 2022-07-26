0ac412c080ad88fe0421696a9d1ac94f7722dae0
Ivoriel <tkivor@gmail.com>
2022-07-26 09:16:01 +0200

    Initial spring boot commit

---
+++ src/main/java/pl/tkosinski/accountingadmin/AccountingAdminApplication.java
@@ -0,0 +1,13 @@
+package pl.tkosinski.accountingadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tkosinski/accountingadmin/AccountingAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tkosinski.accountingadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

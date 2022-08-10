8f32fa770c11f3b4adb422dd62bf53acedf73d28
Ivoriel <tkivor@gmail.com>
2022-08-10 09:29:53 +0200

    Added method to CompanyService interface implementation

--- src/main/java/pl/tkosinski/accountingadmin/company/CompanyServiceImpl.java
+++ src/main/java/pl/tkosinski/accountingadmin/company/CompanyServiceImpl.java
@@ -1,6 +1,12 @@
 package pl.tkosinski.accountingadmin.company;
 
 public class CompanyServiceImpl implements CompanyService {
+
+    @Override
+    public Company saveCompany() {
+        return null;
+    }
+
     @Override
     public Company getCompany() {
         return null;

1882537fab8455634e443e37f57f4ffe23718f0f
Ivoriel <tkivor@gmail.com>
2022-08-14 20:04:05 +0200

    Added Client service implementation

---
+++ src/main/java/pl/tkosinski/accountingadmin/client/ClientServiceImpl.java
@@ -0,0 +1,11 @@
+package pl.tkosinski.accountingadmin.client;
+
+public class ClientServiceImpl implements ClientService {
+
+    public Client save(Client client) {
+        return null;
+    }
+
+
+
+}

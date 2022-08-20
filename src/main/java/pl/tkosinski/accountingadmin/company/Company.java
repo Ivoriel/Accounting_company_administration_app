7c96327b5c74e7baee1950058ecd54c1e9925070
Ivoriel <tkivor@gmail.com>
2022-08-20 18:11:40 +0200

    Added Client to Company object.

--- src/main/java/pl/tkosinski/accountingadmin/company/Company.java
+++ src/main/java/pl/tkosinski/accountingadmin/company/Company.java
@@ -1,8 +1,11 @@
 package pl.tkosinski.accountingadmin.company;
 
+import pl.tkosinski.accountingadmin.client.Client;
+
 public class Company {
 
     private Long id;
     private String name;
+    private Client client;
 
 }

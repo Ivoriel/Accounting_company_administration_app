f67a12b68fc45a8204c3da75485dcb57a6bf4e9a
Ivoriel <tkivor@gmail.com>
2022-08-21 08:22:38 +0200

    CompanyDao object created.

---
+++ src/main/java/pl/tkosinski/accountingadmin/company/CompanyDao.java
@@ -0,0 +1,11 @@
+package pl.tkosinski.accountingadmin.company;
+
+import pl.tkosinski.accountingadmin.client.Client;
+
+public class CompanyDao {
+
+    private Long id;
+    private String name;
+    private Client client;
+
+}

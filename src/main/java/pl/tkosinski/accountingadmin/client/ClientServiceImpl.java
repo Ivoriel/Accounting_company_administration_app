038cbfe62c759fa14382b9a2a41afa87b4319e0c
Ivoriel <tkivor@gmail.com>
2022-08-25 08:57:06 +0200

    CompanyServiceImpl get method added. Annotation added.

--- src/main/java/pl/tkosinski/accountingadmin/client/ClientServiceImpl.java
+++ src/main/java/pl/tkosinski/accountingadmin/client/ClientServiceImpl.java
@@ -2,7 +2,13 @@ package pl.tkosinski.accountingadmin.client;
 
 public class ClientServiceImpl implements ClientService {
 
+    @Override
     public Client save(Client client) {
+        return null;
+    }
+
+    @Override
+    public Client get(Long id) {
         return null;
     }
 

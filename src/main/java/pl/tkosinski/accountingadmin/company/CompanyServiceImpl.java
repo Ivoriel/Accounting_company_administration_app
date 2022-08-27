ed6966d96562eebd83ff72ddfdd8eb8110cd3c82
Ivoriel <tkivor@gmail.com>
2022-08-27 17:58:27 +0200

    Paramethers added to CompanyServiceImpl methods.

--- src/main/java/pl/tkosinski/accountingadmin/company/CompanyServiceImpl.java
+++ src/main/java/pl/tkosinski/accountingadmin/company/CompanyServiceImpl.java
@@ -3,12 +3,12 @@ package pl.tkosinski.accountingadmin.company;
 public class CompanyServiceImpl implements CompanyService {
 
     @Override
-    public Company save() {
+    public Company save(Company company) {
         return null;
     }
 
     @Override
-    public Company get() {
+    public Company get(Long id) {
         return null;
     }
 

038014c3471fdfbdbc5ffdb53621fd65f3570ca7
Ivoriel <tkivor@gmail.com>
2023-02-16 00:14:57 +0100

    ClientDao moved to main repo

---
+++ src/main/java/pl/tkosinski/accountingadmin/client/ClientDao.java
@@ -0,0 +1,34 @@
+package pl.tkosinski.accountingadmin.client;
+
+import pl.kosinski.acaa_dao.Common.BaseDao;
+
+public class ClientDao extends BaseDao {
+
+    private final long id;
+    private String name;
+    private long addressId;
+
+    public ClientDao(long id, String name, long addressId) {
+        this.id = id;
+        this.name = name;
+        this.addressId = addressId;
+    }
+
+    public ClientDao edit(String name, long addressId) {
+        this.name = name;
+        this.addressId = addressId;
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getAddressId() {
+        return addressId;
+    }
+}

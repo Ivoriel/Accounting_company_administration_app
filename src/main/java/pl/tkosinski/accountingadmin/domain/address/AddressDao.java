163da1ca4ea37292b4dc787493f1b64e5b7d0788
Ivoriel <tkivor@gmail.com>
2023-03-24 00:03:07 +0100

    AddressDao moved to main repo

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressDao.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressDao.java
@@ -1,6 +1,6 @@
 package pl.tkosinski.accountingadmin.domain.address;
 
-import pl.kosinski.acaa_dao.Common.BaseDao;
+import pl.tkosinski.accountingadmin.Common.BaseDao;
 
 public class AddressDao extends BaseDao {
     

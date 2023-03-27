a09a7e2acda60d4eeb9012cc52d74aa2ba12b0a0
Ivoriel <tkivor@gmail.com>
2023-03-28 01:43:48 +0200

    ClientRepository moved to main repo

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientRepository.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientRepository.java
@@ -1,6 +1,6 @@
-package pl.tkosinski.accountingadmin.client;
+package pl.tkosinski.accountingadmin.domain.client;
 
-import pl.kosinski.acaa_dao.Common.BaseRepository;
+import pl.tkosinski.accountingadmin.Common.BaseRepository;
 
 import java.util.Optional;
 

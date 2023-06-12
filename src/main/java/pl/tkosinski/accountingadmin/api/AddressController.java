608ef25ecd79b4f264bade17c97202eb5b01a8c9
Ivoriel <tkivor@gmail.com>
2023-06-12 20:50:07 +0200

    Save method added to address controller

--- src/main/java/pl/tkosinski/accountingadmin/api/AddressController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/AddressController.java
@@ -1,7 +1,23 @@
 package pl.tkosinski.accountingadmin.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.tkosinski.accountingadmin.domain.address.AddressFacade;
+import pl.tkosinski.accountingadmin.domain.address.dto.AddressDto;
 
 @Controller
+@RequestMapping("/address")
+@RequiredArgsConstructor
 public class AddressController {
+
+    private final AddressFacade facade;
+
+    @PostMapping("/save")
+    public void save(@RequestParam AddressDto dto) {
+        facade.save(dto);
+    }
+
 }

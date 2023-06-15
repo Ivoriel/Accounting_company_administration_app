2e8560e42dfd155fe0e7d007e7e0da23b2a1b549
Ivoriel <tkivor@gmail.com>
2023-06-16 00:06:44 +0200

    Get method added to address controller

--- src/main/java/pl/tkosinski/accountingadmin/api/ClientController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/ClientController.java
@@ -2,6 +2,9 @@ package pl.tkosinski.accountingadmin.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,5 +21,10 @@ public class ClientController {
     @PostMapping("/save")
     public void save(@RequestParam ClientDto dto) {
         facade.save(dto);
+    }
+
+    @GetMapping("{id}/get")
+    public ClientDto get(@PathVariable Long id) {
+        return facade.get(id);
     }
 }

629c93fccda5c14e01113b9bd5741335517ea7b4
Ivoriel <tkivor@gmail.com>
2023-06-19 00:57:14 +0200

    Save method added to CompanyController

--- src/main/java/pl/tkosinski/accountingadmin/api/CompanyController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/CompanyController.java
@@ -1,7 +1,22 @@
 package pl.tkosinski.accountingadmin.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.tkosinski.accountingadmin.domain.company.CompanyFacade;
+import pl.tkosinski.accountingadmin.domain.company.dto.CompanyDto;
 
 @Controller
+@RequestMapping("/company")
+@RequiredArgsConstructor
 public class CompanyController {
+
+    private final CompanyFacade facade;
+
+    @PostMapping("/save")
+    public void save(@RequestParam CompanyDto dto) {
+        facade.save(dto);
+    }
 }

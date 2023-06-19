77a511e6c86f1e0c9338ee539b12b324e789c3df
Ivoriel <tkivor@gmail.com>
2023-06-20 00:46:58 +0200

    Get method added to CompanyController

--- src/main/java/pl/tkosinski/accountingadmin/api/CompanyController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/CompanyController.java
@@ -2,6 +2,8 @@ package pl.tkosinski.accountingadmin.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,5 +20,10 @@ public class CompanyController {
     @PostMapping("/save")
     public void save(@RequestParam CompanyDto dto) {
         facade.save(dto);
+    }
+
+    @GetMapping("/{id}/get")
+    public CompanyDto get(@PathVariable Long id) {
+        return facade.get(id);
     }
 }

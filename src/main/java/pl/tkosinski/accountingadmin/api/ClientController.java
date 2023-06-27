b858b18e0ae57b219222995b44943cb904178a7f
Ivoriel <tkivor@gmail.com>
2023-06-27 11:49:15 +0200

    Params and routing fixes

--- src/main/java/pl/tkosinski/accountingadmin/api/ClientController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/ClientController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import pl.tkosinski.accountingadmin.domain.client.ClientFacade;
 import pl.tkosinski.accountingadmin.domain.client.dto.ClientDto;
 
@@ -19,16 +19,16 @@ public class ClientController {
     private final ClientFacade facade;
 
     @PostMapping("/save")
-    public void save(@RequestParam ClientDto dto) {
+    public void save(@RequestBody ClientDto dto) {
         facade.save(dto);
     }
 
-    @GetMapping("{id}/get")
+    @GetMapping("/{id}/get")
     public ClientDto get(@PathVariable Long id) {
         return facade.get(id);
     }
 
-    @DeleteMapping("{id}/delete")
+    @DeleteMapping("/{id}/delete")
     public void delete(@PathVariable Long id) {
         facade.delete(id);
     }

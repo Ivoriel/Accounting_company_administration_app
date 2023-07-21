6ce96d77da617e1e15be4de74d784b46d4416bb6
Ivoriel <tkivor@gmail.com>
2023-07-21 14:12:19 +0200

    Switched AddressDto to utilize @Value and @Builder annotations instead of @Data

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressMapper.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressMapper.java
@@ -5,15 +5,15 @@ import pl.tkosinski.accountingadmin.domain.address.dto.AddressDto;
 public class AddressMapper {
 
     public static AddressDto toDto(AddressDao addressDao) {
-        AddressDto addressDto = new AddressDto();
-        addressDto.setId(addressDao.getId());
-        addressDto.setCountry(addressDao.getCountry());
-        addressDto.setMunicipality(addressDao.getMunicipality());
-        addressDto.setRegion(addressDao.getRegion());
-        addressDto.setZipCode(addressDao.getZipCode());
-        addressDto.setStreet(addressDao.getStreet());
-        addressDto.setBuildingNumber(addressDao.getBuildingNumber());
-        addressDto.setAdditionalIdentifier(addressDao.getAdditionalIdentifier());
-        return addressDto;
+        return AddressDto.builder()
+                .id(addressDao.getId())
+                .country(addressDao.getCountry())
+                .municipality(addressDao.getMunicipality())
+                .region(addressDao.getRegion())
+                .zipCode(addressDao.getZipCode())
+                .street(addressDao.getStreet())
+                .buildingNumber(addressDao.getBuildingNumber())
+                .additionalIdentifier(addressDao.getAdditionalIdentifier())
+                .build();
     }
 }

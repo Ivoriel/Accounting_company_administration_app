10181e9c63c52842a107fca88970684fdc22c02a
Ivor <tkivor@gmail.com>
2023-08-12 13:16:13 +0200

    Package name fixed

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressRepository.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressRepository.java
@@ -1,6 +1,6 @@
 package pl.tkosinski.accountingadmin.domain.address;
 
-import pl.tkosinski.accountingadmin.Common.BaseRepository;
+import pl.tkosinski.accountingadmin.common.BaseRepository;
 
 import java.util.Optional;
 

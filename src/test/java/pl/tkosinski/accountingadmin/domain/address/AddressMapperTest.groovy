203bf32183c5f2a36954982642f1c15134cf5cda
Ivor <tkivor@gmail.com>
2023-09-05 00:00:36 +0200

    Added address mapper test

---
+++ src/test/java/pl/tkosinski/accountingadmin/domain/address/AddressMapperTest.groovy
@@ -0,0 +1,32 @@
+package pl.tkosinski.accountingadmin.domain.address
+
+import spock.lang.Specification
+
+class AddressMapperTest extends Specification {
+
+    AddressMapper mapper = new AddressMapper()
+
+    def "should map address entity to dto"() {
+        def dao = AddressDao.builder()
+                .country("Polska")
+                .municipality("Toruń")
+                .region("kujawsko-pomorskie")
+                .zipCode("87-100")
+                .street("Jasna")
+                .buildingNumber("1")
+                .additionalIdentifier("2p")
+                .build()
+
+        when:
+        def dto = mapper.toDto(dao)
+
+        then:
+        dto.country == dao.country
+        dto.municipality == dao.municipality
+        dto.region == dao.region
+        dto.zipCode == dao.zipCode
+        dto.street == dao.street
+        dto.buildingNumber == dao.buildingNumber
+        dto.additionalIdentifier == dao.additionalIdentifier
+    }
+}

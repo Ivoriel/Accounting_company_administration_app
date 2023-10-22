7537b59243539e94da412a1165bd09340fcc122b
Ivor <tkivor@gmail.com>
2023-10-23 00:00:38 +0200

    Removed redundant explicit method

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyDao.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyDao.java
@@ -26,9 +26,4 @@ public class CompanyDao extends BaseDao {
         this.addressId = addressId;
         return this;
     }
-
-    @Override
-    public long getId() {
-        return id;
-    }
 }

47f6edf4475a6de7c0c62b110fbec4cb3fd63710
Ivor <tkivor@gmail.com>
2023-10-24 02:57:48 +0200

    Defined parameter and added methods

--- src/main/java/pl/tkosinski/accountingadmin/common/BaseRepository.java
+++ src/main/java/pl/tkosinski/accountingadmin/common/BaseRepository.java
@@ -1,4 +1,18 @@
 package pl.tkosinski.accountingadmin.common;
 
-public interface BaseRepository {
+import java.util.Optional;
+
+public interface BaseRepository<T extends BaseDao> {
+
+    T save(T dao);
+
+    Optional<T> get(long id);
+
+    void delete(long id);
+
+    int size();
+
+    T generate();
+
+    T generateAndSave();
 }

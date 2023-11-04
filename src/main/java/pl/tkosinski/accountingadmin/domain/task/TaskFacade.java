e309e473192ac4b2d4d4370eaa01d27cbdccdb6d
Ivor <tkivor@gmail.com>
2023-11-04 13:24:57 +0100

    Save method implemented

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskFacade.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskFacade.java
@@ -2,10 +2,25 @@ package pl.tkosinski.accountingadmin.domain.task;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.tkosinski.accountingadmin.domain.task.dto.TaskDto;
 
 @Component
 @RequiredArgsConstructor
 public class TaskFacade {
 
     private final TaskRepository repository;
+
+    public void save(TaskDto dto) {
+        repository.get(dto.getId()).ifPresentOrElse(it -> update(it, dto), () -> create(dto));
+    }
+
+    private void update(TaskDao dao, TaskDto dto) {
+        repository.save(dao.edit());
+    }
+
+    private void create(TaskDto dto) {
+        repository.save(TaskDao.builder()
+                .id(repository.size())
+                .build());
+    }
 }

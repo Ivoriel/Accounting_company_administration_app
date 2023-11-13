84e0713158f50616370c47e0510e80fe6508ff34
Ivor <tkivor@gmail.com>
2023-11-13 01:08:08 +0100

    TaskController added

---
+++ src/main/java/pl/tkosinski/accountingadmin/api/TaskController.java
@@ -0,0 +1,40 @@
+package pl.tkosinski.accountingadmin.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pl.tkosinski.accountingadmin.domain.company.dto.CompanyDto;
+import pl.tkosinski.accountingadmin.domain.task.TaskFacade;
+import pl.tkosinski.accountingadmin.domain.task.dto.TaskDto;
+
+@RestController
+@RequestMapping("/task")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskFacade facade;
+
+    @PostMapping("/save")
+    public void save(@RequestBody TaskDto dto) {
+        facade.save(dto);
+    }
+
+    @GetMapping("/{id}/get")
+    public TaskDto get(@PathVariable Long id) {
+        return facade.get(id);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public void delete(@PathVariable Long id) {
+        facade.delete(id);
+    }
+
+    @GetMapping("/generate")
+    public TaskDto generate() {
+        return facade.generate();
+    }
+
+    @GetMapping("/generate-and-save")
+    public TaskDto generateAndSave() {
+        return facade.generateAndSave();
+    }
+}

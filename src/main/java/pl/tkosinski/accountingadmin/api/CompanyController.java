de57433ac6fa711953d8140b9a44c0c977fe39d0
Ivor <tkivor@gmail.com>
2023-11-16 00:15:20 +0100

    Swapped annotation for swagger

--- src/main/java/pl/tkosinski/accountingadmin/api/CompanyController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/CompanyController.java
@@ -1,17 +1,11 @@
 package pl.tkosinski.accountingadmin.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.tkosinski.accountingadmin.domain.company.CompanyFacade;
 import pl.tkosinski.accountingadmin.domain.company.dto.CompanyDto;
 
-@Controller
+@RestController
 @RequestMapping("/company")
 @RequiredArgsConstructor
 public class CompanyController {

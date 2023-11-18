c5ebca1bd642bc7d64f758ce9ecaace2baa066f6
Ivor <tkivor@gmail.com>
2023-11-18 12:08:06 +0100

    Swapped annotation for swagger

--- src/main/java/pl/tkosinski/accountingadmin/api/AddressController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/AddressController.java
@@ -1,17 +1,11 @@
 package pl.tkosinski.accountingadmin.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.tkosinski.accountingadmin.domain.address.AddressFacade;
 import pl.tkosinski.accountingadmin.domain.address.dto.AddressDto;
 
-@Controller
+@RestController
 @RequestMapping("/address")
 @RequiredArgsConstructor
 public class AddressController {

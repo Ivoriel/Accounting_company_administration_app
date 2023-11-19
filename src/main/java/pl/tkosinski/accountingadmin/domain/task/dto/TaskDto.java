aa39fb9a01f831a8da3981e1fa950b3b33e5fc42
Ivor <tkivor@gmail.com>
2023-11-20 00:07:36 +0100

    Added parameters to task dto

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskDto.java
@@ -3,9 +3,14 @@ package pl.tkosinski.accountingadmin.domain.task.dto;
 import lombok.Builder;
 import lombok.Value;
 
+import java.time.LocalDateTime;
+
 @Value
 @Builder
 public class TaskDto {
 
     long id;
+    LocalDateTime start;
+    LocalDateTime end;
+    String comment;
 }

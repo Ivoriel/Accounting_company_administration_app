b97e345a31bb445600fb5ef4937f010d2faeb394
Ivor <tkivor@gmail.com>
2023-11-19 15:15:04 +0100

    Added parameters to task dao

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskDao.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskDao.java
@@ -4,11 +4,16 @@ import lombok.Builder;
 import lombok.Getter;
 import pl.tkosinski.accountingadmin.common.BaseDao;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class TaskDao extends BaseDao {
 
     private final long id;
+    private final LocalDateTime start;
+    private final LocalDateTime end;
+    private final String comment;
 
     public TaskDao edit() {
         return this;

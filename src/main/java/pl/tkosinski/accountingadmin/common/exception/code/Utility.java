0fdc71951f641d3130cbbaecd559ef4a22b59900
Ivor <tkivor@gmail.com>
2024-02-08 17:38:37 +0100

    Created utility enum

---
+++ src/main/java/pl/tkosinski/accountingadmin/common/exception/code/Utility.java
@@ -0,0 +1,12 @@
+package pl.tkosinski.accountingadmin.common.exception.code;
+
+public enum Utility {
+
+    UTILITY_CLASS("Utility class");
+
+    public final String message;
+
+    private Utility(String message) {
+        this.message = message;
+    }
+}

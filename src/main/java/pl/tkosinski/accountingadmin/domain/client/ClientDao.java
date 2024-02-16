0bbb52291dfa88c5127d150dda27396cb1c50e05
Ivor <tkivor@gmail.com>
2024-02-16 17:40:56 +0100

    Switched to FullName object

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientDao.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientDao.java
@@ -3,26 +3,24 @@ package pl.tkosinski.accountingadmin.domain.client;
 import lombok.Builder;
 import lombok.Getter;
 import pl.tkosinski.accountingadmin.common.BaseDao;
+import pl.tkosinski.accountingadmin.common.FullName;
 
 @Builder
 @Getter
 public class ClientDao extends BaseDao {
 
     private final long id;
-    private String givenName;
-    private String lastName;
+    private FullName name;
     private long addressId;
 
-    public ClientDao(long id, String givenName, String lastName, long addressId) {
+    public ClientDao(long id, FullName name, long addressId) {
         this.id = id;
-        this.givenName = givenName;
-        this.lastName = lastName;
+        this.name = name;
         this.addressId = addressId;
     }
 
-    public ClientDao edit(String givenName, String lastName, long addressId) {
-        this.givenName = givenName;
-        this.lastName = lastName;
+    public ClientDao edit(FullName name, long addressId) {
+        this.name = name;
         this.addressId = addressId;
         return this;
     }

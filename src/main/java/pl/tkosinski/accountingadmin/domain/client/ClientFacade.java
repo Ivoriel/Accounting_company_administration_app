786d94d54d3f5220f92eebb7836bdca6adf84030
Ivor <tkivor@gmail.com>
2024-02-18 01:07:32 +0100

    Switched to FullName object

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientFacade.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientFacade.java
@@ -33,14 +33,13 @@ public class ClientFacade {
     }
 
     private void updateClient(ClientDao dao, ClientDto dto) {
-        clientRepository.save(dao.edit(dto.getGivenName(), dao.getLastName(), dto.getAddressId()));
+        clientRepository.save(dao.edit(dto.getName(), dto.getAddressId()));
     }
 
     private void createClient(ClientDto dto) {
         clientRepository.save(ClientDao.builder()
                 .id(clientRepository.size())
-                .givenName(dto.getGivenName())
-                .lastName(dto.getLastName())
+                .name(dto.getName())
                 .addressId(dto.getAddressId())
                 .build());
     }

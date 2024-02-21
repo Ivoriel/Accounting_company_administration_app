df292d9a73b14cf56330b830d43bd797bc4d1a35
Ivor <tkivor@gmail.com>
2024-02-21 17:44:10 +0100

    Created client sample trait

---
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesClientSample.groovy
@@ -0,0 +1,15 @@
+package pl.tkosinski.accountingadmin.domain.sample
+
+import pl.tkosinski.accountingadmin.common.FullName
+import pl.tkosinski.accountingadmin.domain.client.ClientDao
+
+trait UsesClientSample {
+
+    ClientDao.ClientDaoBuilder clientDaoSample() {
+        ClientDao.builder()
+                .id(16)
+                .name(FullName.ofValue("Teodor", "Nowak"))
+                .addressId(165)
+    }
+
+}

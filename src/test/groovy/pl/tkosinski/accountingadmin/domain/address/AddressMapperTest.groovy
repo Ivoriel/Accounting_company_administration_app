a5dac4608827e1c3a21f4edbde6b73f1f929da73
Ivor <tkivor@gmail.com>
2024-02-25 00:12:03 +0100

    Switched to use sample trait

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/address/AddressMapperTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/address/AddressMapperTest.groovy
@@ -1,22 +1,14 @@
 package pl.tkosinski.accountingadmin.domain.address
 
+import pl.tkosinski.accountingadmin.domain.sample.UsesAddressSample
 import spock.lang.Specification
 
-class AddressMapperTest extends Specification {
+class AddressMapperTest extends Specification implements UsesAddressSample {
 
     AddressMapper mapper = new AddressMapper()
 
     def "should map address entity to dto"() {
-        def dao = AddressDao.builder()
-                .id(17)
-                .country("Polska")
-                .municipality("Toruń")
-                .region("kujawsko-pomorskie")
-                .zipCode("87-100")
-                .street("Jasna")
-                .buildingNumber("1")
-                .additionalIdentifier("2p")
-                .build()
+        def dao = addressDaoSample().build()
 
         when:
         def dto = mapper.toDto(dao)

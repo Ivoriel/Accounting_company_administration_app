9aeaf06e6c68b3342944f7a30ccc798b7e7b684d
Ivor <tkivor@gmail.com>
2024-02-27 17:21:44 +0100

    Switched to use sample trait

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/client/ClientMapperTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/client/ClientMapperTest.groovy
@@ -1,19 +1,16 @@
 package pl.tkosinski.accountingadmin.domain.client
 
 import pl.tkosinski.accountingadmin.common.FullName
+import pl.tkosinski.accountingadmin.domain.sample.UsesClientSample
 import spock.lang.Specification
 
-class ClientMapperTest extends Specification {
+class ClientMapperTest extends Specification implements UsesClientSample {
 
     ClientMapper mapper = new ClientMapper()
 
     def "should map client entity to dto"() {
         given:
-        def dao = ClientDao.builder()
-                .id(16)
-                .name(FullName.ofValue("Teodor", "Nowak"))
-                .addressId(165)
-                .build()
+        def dao = clientDaoSample().build()
 
         when:
         def dto = mapper.toDto(dao)

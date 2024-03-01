0a855c20715aa472ecced0dfd9538c6833b8ae6c
Ivor <tkivor@gmail.com>
2024-03-01 15:40:22 +0100

    Switched to use sample trait

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/company/CompanyRepositoryMockTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/company/CompanyRepositoryMockTest.groovy
@@ -4,9 +4,10 @@ import pl.tkosinski.accountingadmin.domain.address.AddressFacade
 import pl.tkosinski.accountingadmin.domain.address.AddressRepositoryMock
 import pl.tkosinski.accountingadmin.domain.client.ClientFacade
 import pl.tkosinski.accountingadmin.domain.client.ClientRepositoryMock
+import pl.tkosinski.accountingadmin.domain.sample.UsesCompanySample
 import spock.lang.Specification
 
-class CompanyRepositoryMockTest extends Specification {
+class CompanyRepositoryMockTest extends Specification implements UsesCompanySample {
 
     AddressFacade addressFacade = new AddressFacade(new AddressRepositoryMock(new HashMap()))
     CompanyRepository repository = new CompanyRepositoryMock(new HashMap(), addressFacade,
@@ -22,11 +23,7 @@ class CompanyRepositoryMockTest extends Specification {
 
     def "should create and save company"() {
         given:
-        var companyToSave = CompanyDao.builder()
-                .name("company test name")
-                .clientId(1)
-                .addressId(1)
-                .build()
+        var companyToSave = companyDaoSample().build()
 
         when:
         var savedDao = repository.save(companyToSave)
@@ -39,11 +36,7 @@ class CompanyRepositoryMockTest extends Specification {
 
     def "should get company"() {
         given:
-        var companyToSave = CompanyDao.builder()
-                .name("company test name")
-                .clientId(1)
-                .addressId(1)
-                .build()
+        var companyToSave = companyDaoSample().build()
         var savedDaoId = repository.save(companyToSave).id
 
         when:
@@ -57,11 +50,7 @@ class CompanyRepositoryMockTest extends Specification {
 
     def "should delete company"() {
         given:
-        var companyToSave = CompanyDao.builder()
-                .name("company test name")
-                .clientId(1)
-                .addressId(1)
-                .build()
+        var companyToSave = companyDaoSample().build()
         var savedDaoId = repository.save(companyToSave).id
 
         when:
@@ -73,11 +62,7 @@ class CompanyRepositoryMockTest extends Specification {
 
     def "should return repository size"() {
         when:
-        repository.save(CompanyDao.builder()
-                .name("company test name")
-                .clientId(1)
-                .addressId(1)
-                .build())
+        repository.save(companyDaoSample().build())
 
         then:
         repository.size() == 1

746494b07be67e60a04486813198127f32193dd9
Ivor <tkivor@gmail.com>
2024-03-06 07:45:01 +0100

    Created Id object

---
+++ src/main/java/pl/tkosinski/accountingadmin/common/model/Id.java
@@ -0,0 +1,18 @@
+package pl.tkosinski.accountingadmin.common.model;
+
+public class Id {
+    
+    private final long value;
+
+    private Id(long value) {
+        this.value = value;
+    }
+
+    public Id ofValue(long value) {
+        return new Id(value);
+    }
+
+    public long getValue() {
+        return value;
+    }
+}

1c115007518e5b37f382239ec10ece543f2d8018
Ivor <tkivor@gmail.com>
2024-03-21 18:40:55 +0100

    Switched to Id object

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyDao.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyDao.java
@@ -3,24 +3,25 @@ package pl.tkosinski.accountingadmin.domain.company;
 import lombok.Builder;
 import lombok.Getter;
 import pl.tkosinski.accountingadmin.common.BaseDao;
+import pl.tkosinski.accountingadmin.common.model.Id;
 
 @Getter
 @Builder
 public class CompanyDao extends BaseDao {
 
-    private final long id;
+    private final Id id;
     private String name;
-    private long clientId;
-    private long addressId;
+    private Id clientId;
+    private Id addressId;
 
-    public CompanyDao(long id, String name, long clientId, long addressId) {
+    public CompanyDao(Id id, String name, Id clientId, Id addressId) {
         this.id = id;
         this.name = name;
         this.clientId = clientId;
         this.addressId = addressId;
     }
 
-    public CompanyDao edit(String name, long clientId, long addressId) {
+    public CompanyDao edit(String name, Id clientId, Id addressId) {
         this.name = name;
         this.clientId = clientId;
         this.addressId = addressId;

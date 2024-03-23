6066f028bfa285cfd38bd33e9cca2f78aedde1c3
Ivor <tkivor@gmail.com>
2024-03-24 00:49:52 +0100

    Switched to Id object

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyRepositoryMock.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyRepositoryMock.java
@@ -3,6 +3,7 @@ package pl.tkosinski.accountingadmin.domain.company;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.domain.address.AddressFacade;
 import pl.tkosinski.accountingadmin.domain.client.ClientFacade;
 
@@ -18,7 +19,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @AllArgsConstructor
 class CompanyRepositoryMock implements CompanyRepository{
 
-    HashMap<Long, CompanyDao> companyDb;
+    HashMap<Id, CompanyDao> companyDb;
     AddressFacade addressFacade;
     ClientFacade clientFacade;
 
@@ -35,12 +36,12 @@ class CompanyRepositoryMock implements CompanyRepository{
     }
 
     @Override
-    public Optional<CompanyDao> get(long id) {
+    public Optional<CompanyDao> get(Id id) {
         return Optional.ofNullable(companyDb.get(id));
     }
 
     @Override
-    public void delete(long id) {
+    public void delete(Id id) {
         companyDb.remove(id);
     }
 
@@ -61,7 +62,7 @@ class CompanyRepositoryMock implements CompanyRepository{
 
     private CompanyDao generateCompany() {
         return CompanyDao.builder()
-                .id(size())
+                .id(Id.ofValue(size()))
                 .name(generateCompanyName())
                 .clientId(clientFacade.generate().getId())
                 .addressId(addressFacade.generate().getId())
@@ -70,7 +71,7 @@ class CompanyRepositoryMock implements CompanyRepository{
 
     private void populateCompanyDb() {
         for (long i = 1; i < 10; i++) {
-            companyDb.put(i, generateAndSave());
+            companyDb.put(Id.ofValue(i), generateAndSave());
         }
     }
 

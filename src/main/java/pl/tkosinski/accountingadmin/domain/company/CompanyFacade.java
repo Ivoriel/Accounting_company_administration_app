24bf0088c149b0d7b4f0c057f8533df9885d3122
Ivor <tkivor@gmail.com>
2024-03-25 21:11:32 +0100

    Switched to Id object

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyFacade.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyFacade.java
@@ -2,6 +2,7 @@ package pl.tkosinski.accountingadmin.domain.company;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.domain.company.dto.CompanyDto;
 
 import java.util.NoSuchElementException;
@@ -16,11 +17,11 @@ public class CompanyFacade {
         repository.get(dto.getId()).ifPresentOrElse(it -> updateCompany(it, dto), () -> createCompany(dto));
     }
 
-    public CompanyDto get(Long id) {
+    public CompanyDto get(Id id) {
         return CompanyMapper.toDto(repository.get(id).orElseThrow(NoSuchElementException::new));
     }
 
-    public void delete(Long id) {
+    public void delete(Id id) {
         repository.delete(id);
     }
 
@@ -38,7 +39,7 @@ public class CompanyFacade {
 
     private void createCompany(CompanyDto dto) {
         repository.save(CompanyDao.builder()
-                .id(repository.size())
+                .id(Id.ofValue(repository.size()))
                 .name(dto.getName())
                 .clientId(dto.getClientId())
                 .addressId(dto.getAddressId())

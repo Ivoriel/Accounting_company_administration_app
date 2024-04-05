69ce0d566f2a7bba4c63ba04884db9ab4ade841d
Ivor <tkivor@gmail.com>
2024-04-06 00:16:48 +0200

    Switched to InstitutionName value object

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyDao.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyDao.java
@@ -4,24 +4,25 @@ import lombok.Builder;
 import lombok.Getter;
 import pl.tkosinski.accountingadmin.common.BaseDao;
 import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.common.model.InstitutionName;
 
 @Getter
 @Builder
 public class CompanyDao extends BaseDao {
 
     private final Id id;
-    private String name;
+    private InstitutionName name;
     private Id clientId;
     private Id addressId;
 
-    public CompanyDao(Id id, String name, Id clientId, Id addressId) {
+    public CompanyDao(Id id, InstitutionName name, Id clientId, Id addressId) {
         this.id = id;
         this.name = name;
         this.clientId = clientId;
         this.addressId = addressId;
     }
 
-    public CompanyDao edit(String name, Id clientId, Id addressId) {
+    public CompanyDao edit(InstitutionName name, Id clientId, Id addressId) {
         this.name = name;
         this.clientId = clientId;
         this.addressId = addressId;

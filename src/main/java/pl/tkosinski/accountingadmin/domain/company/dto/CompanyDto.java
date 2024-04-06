664179c211f74ea5481f4fb55d346447b90f904e
Ivor <tkivor@gmail.com>
2024-04-07 00:24:34 +0200

    Switched to InstitutionName value object

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/dto/CompanyDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/dto/CompanyDto.java
@@ -3,13 +3,14 @@ package pl.tkosinski.accountingadmin.domain.company.dto;
 import lombok.Builder;
 import lombok.Value;
 import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.common.model.InstitutionName;
 
 @Value
 @Builder
 public class CompanyDto {
 
     Id id;
-    String name;
+    InstitutionName name;
     Id clientId;
     Id addressId;
 

49806f24de0e569ffdfe3e593ac715fd2969d817
Ivor <tkivor@gmail.com>
2024-04-10 00:01:27 +0200

    Switched to Comment value object

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskDao.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskDao.java
@@ -3,6 +3,7 @@ package pl.tkosinski.accountingadmin.domain.task;
 import lombok.Builder;
 import lombok.Getter;
 import pl.tkosinski.accountingadmin.common.BaseDao;
+import pl.tkosinski.accountingadmin.common.model.Comment;
 import pl.tkosinski.accountingadmin.common.model.Id;
 
 import java.time.LocalDateTime;
@@ -14,9 +15,9 @@ public class TaskDao extends BaseDao {
     private final Id id;
     private LocalDateTime start;
     private LocalDateTime end;
-    private String comment;
+    private Comment comment;
 
-    public TaskDao edit(LocalDateTime start, LocalDateTime end, String comment) {
+    public TaskDao edit(LocalDateTime start, LocalDateTime end, Comment comment) {
         this.start = start;
         this.end = end;
         this.comment = comment;

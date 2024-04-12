7d21fa689c0f6befb6b0838912de2c2bf1198519
Ivor <tkivor@gmail.com>
2024-04-12 20:47:28 +0200

    Switched to value objects

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesClientSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesClientSample.groovy
@@ -1,15 +1,16 @@
 package pl.tkosinski.accountingadmin.domain.sample
 
 import pl.tkosinski.accountingadmin.common.model.FullName
+import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.client.ClientDao
 
 trait UsesClientSample {
 
     ClientDao.ClientDaoBuilder clientDaoSample() {
         ClientDao.builder()
-                .id(16)
+                .id(Id.ofValue(16))
                 .name(FullName.ofValue("Teodor", "Nowak"))
-                .addressId(165)
+                .addressId(Id.ofValue(165))
     }
 
 }

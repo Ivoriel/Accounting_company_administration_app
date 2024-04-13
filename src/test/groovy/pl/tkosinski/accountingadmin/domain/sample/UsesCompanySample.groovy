69fa7eda0f83d216132cfce79640fcfa8b75c352
Ivor <tkivor@gmail.com>
2024-04-13 11:17:42 +0200

    Switched to value objects

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesCompanySample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesCompanySample.groovy
@@ -1,15 +1,17 @@
 package pl.tkosinski.accountingadmin.domain.sample
 
+import pl.tkosinski.accountingadmin.common.model.Id
+import pl.tkosinski.accountingadmin.common.model.InstitutionName
 import pl.tkosinski.accountingadmin.domain.company.CompanyDao
 
 trait UsesCompanySample {
 
     CompanyDao.CompanyDaoBuilder companyDaoSample() {
         CompanyDao.builder()
-                .id(616)
-                .name("Test company name")
-                .addressId(919)
-                .clientId(818)
+                .id(Id.ofValue(616))
+                .name(InstitutionName.ofValue("Test company name"))
+                .addressId(Id.ofValue(919))
+                .clientId(Id.ofValue(818))
     }
 
 }

3761f0496251f4c9821aaeb5fe8a55963b325c0a
Ivor <tkivor@gmail.com>
2024-04-14 11:44:49 +0200

    Switched to value objects

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesTaskSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesTaskSample.groovy
@@ -1,5 +1,6 @@
 package pl.tkosinski.accountingadmin.domain.sample
 
+import pl.tkosinski.accountingadmin.common.model.Comment
 import pl.tkosinski.accountingadmin.domain.task.TaskDao
 
 import java.time.LocalDateTime
@@ -10,7 +11,7 @@ trait UsesTaskSample {
         TaskDao.builder()
                 .start(LocalDateTime.now().minusMinutes(60))
                 .end(LocalDateTime.now().minusMinutes(5))
-                .comment("test comment")
+                .comment(Comment.ofValue( "test comment"))
     }
 
 }

8941b1c68e382b5560cabc9c7207ead52e201ae6
Ivor <tkivor@gmail.com>
2024-04-16 16:19:26 +0200

    Switched to value objects

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesUserSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesUserSample.groovy
@@ -1,13 +1,14 @@
 package pl.tkosinski.accountingadmin.domain.sample
 
 import pl.tkosinski.accountingadmin.common.model.FullName
+import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.user.UserDao
 
 trait UsesUserSample {
 
     UserDao.UserDaoBuilder userDaoSample() {
         UserDao.builder()
-                .id(616)
+                .id(Id.ofValue(616))
                 .name(FullName.ofValue("Teodor", "Nowak"))
     }
 }

233ee2ef0174cf514d6f81027de1f4dda4896fab
Ivor <tkivor@gmail.com>
2024-04-23 20:46:03 +0200

    Switched to value object

--- src/main/java/pl/tkosinski/accountingadmin/api/UserController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/UserController.java
@@ -2,6 +2,7 @@ package pl.tkosinski.accountingadmin.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.domain.user.UserFacade;
 import pl.tkosinski.accountingadmin.domain.user.dto.UserDto;
 
@@ -18,12 +19,12 @@ public class UserController {
     }
 
     @GetMapping("/{id}/get")
-    public UserDto get(@PathVariable Long id) {
+    public UserDto get(@PathVariable Id id) {
         return facade.get(id);
     }
 
     @DeleteMapping("/{id}/delete")
-    public void delete(@PathVariable Long id) {
+    public void delete(@PathVariable Id id) {
         facade.delete(id);
     }
 

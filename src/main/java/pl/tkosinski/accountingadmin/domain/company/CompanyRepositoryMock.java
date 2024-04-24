bbfb9fe1878be51b4cd47e0f34a120fc564ca411
Ivor <tkivor@gmail.com>
2024-04-24 20:53:27 +0200

    Switched to value object

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyRepositoryMock.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyRepositoryMock.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.common.model.InstitutionName;
 import pl.tkosinski.accountingadmin.domain.address.AddressFacade;
 import pl.tkosinski.accountingadmin.domain.client.ClientFacade;
 
@@ -75,11 +76,11 @@ class CompanyRepositoryMock implements CompanyRepository{
         }
     }
 
-    private String generateCompanyName() {
+    private InstitutionName generateCompanyName() {
         String[] names = {"Januszex Janusz Typowy", "PolExport sp. z o.o.", "BiznesEx s.c.", "PolBiznes SA",
                 "ExKosmos s.j.", "Mydło i Powidło Jan Nowak", "Warzywa i owoca Ewelina Bluszcz", "PHU Kliper s.c.",
                 "Story-Train Bill Smith", "Gilgamesh sp. z o.o.", "Enkidu SA", "HusariaPol Marian Kmieć"};
-        return names[generateRandomInt(names.length - 1)];
+        return InstitutionName.ofValue(names[generateRandomInt(names.length - 1)]);
     }
 
     private int generateRandomInt(int max) {

acb12b6ac00d9fc15b931af4caa32cb4136f2111
Ivor <tkivor@gmail.com>
2024-04-28 07:56:41 +0200

    Added validate method

--- src/main/java/pl/tkosinski/accountingadmin/common/model/Comment.java
+++ src/main/java/pl/tkosinski/accountingadmin/common/model/Comment.java
@@ -5,7 +5,7 @@ import lombok.Value;
 import java.io.Serializable;
 
 @Value
-public class Comment implements Serializable {
+public class Comment implements Serializable, Validatable {
 
     String value;
 
@@ -15,5 +15,11 @@ public class Comment implements Serializable {
 
     public static Comment ofValue(String value) {
         return new Comment(value);
+    }
+
+    public void validate() {
+        if (value == null) {
+            throw new UnsupportedOperationException();
+        }
     }
 }

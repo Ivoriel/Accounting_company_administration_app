a4c2882bdda07c583c318dc5762e184df3c6fc18
Ivor <tkivor@gmail.com>
2024-05-08 22:31:46 +0200

    Added performer to task

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesTaskSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesTaskSample.groovy
@@ -1,6 +1,7 @@
 package pl.tkosinski.accountingadmin.domain.sample
 
 import pl.tkosinski.accountingadmin.common.model.Comment
+import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.task.TaskDao
 
 import java.time.LocalDateTime
@@ -9,6 +10,7 @@ trait UsesTaskSample {
 
     TaskDao.TaskDaoBuilder taskDaoSample() {
         TaskDao.builder()
+                .performerId(Id.ofValue(808))
                 .start(LocalDateTime.now().minusMinutes(60))
                 .end(LocalDateTime.now().minusMinutes(5))
                 .comment(Comment.ofValue( "test comment"))

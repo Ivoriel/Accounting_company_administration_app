328a0afee77a7431f1c8c95132656f8f11940eb8
Ivor <tkivor@gmail.com>
2024-06-05 01:03:15 +0200

    Added role to user

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/UserDao.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/UserDao.java
@@ -5,16 +5,19 @@ import lombok.Getter;
 import pl.tkosinski.accountingadmin.common.BaseDao;
 import pl.tkosinski.accountingadmin.common.model.FullName;
 import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.common.model.Role;
 
 @Getter
 @Builder
 public class UserDao extends BaseDao {
 
     private final Id id;
+    private Role role;
     private FullName name;
 
-    public UserDao(Id id, FullName name) {
+    public UserDao(Id id, Role role, FullName name) {
         this.id = id;
+        this.role = role;
         this.name = name;
     }
 

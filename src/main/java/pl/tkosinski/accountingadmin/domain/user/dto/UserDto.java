8b250cf7878fb4ba6b806a3cfdb2503a005253fb
Ivor <tkivor@gmail.com>
2024-06-09 11:53:29 +0200

    Role attribute added

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserDto.java
@@ -4,11 +4,13 @@ import lombok.Builder;
 import lombok.Value;
 import pl.tkosinski.accountingadmin.common.model.FullName;
 import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.common.model.Role;
 
 @Value
 @Builder
 public class UserDto {
 
     Id id;
+    Role role;
     FullName name;
 }

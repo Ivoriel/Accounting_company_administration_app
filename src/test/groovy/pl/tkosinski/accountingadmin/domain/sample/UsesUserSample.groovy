1a0a3da0d13b87c893e78e2656d43d53f852c3f9
Ivor <tkivor@gmail.com>
2024-06-17 17:47:25 +0200

    Role attribute added

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesUserSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesUserSample.groovy
@@ -4,11 +4,14 @@ import pl.tkosinski.accountingadmin.common.model.FullName
 import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.user.UserDao
 
+import static pl.tkosinski.accountingadmin.common.model.Role.EMPLOYEE
+
 trait UsesUserSample {
 
     UserDao.UserDaoBuilder userDaoSample(def args = null) {
         UserDao.builder()
                 .id(Id.ofValue(args?.id ?: 16))
+                .role(EMPLOYEE)
                 .name(FullName.ofValue("Teodor", "Nowak"))
     }
 }

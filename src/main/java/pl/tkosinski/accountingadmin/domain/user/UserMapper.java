a84d5aaa3a1a11729abc8c9135b1743f872c327e
Ivor <tkivor@gmail.com>
2024-06-18 17:40:46 +0200

    Role attribute added

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/UserMapper.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/UserMapper.java
@@ -7,6 +7,7 @@ public class UserMapper {
     public static UserDto toDto(UserDao dao) {
         return UserDto.builder()
                 .id(dao.getId())
+                .role(dao.getRole())
                 .name(dao.getName())
                 .build();
     }

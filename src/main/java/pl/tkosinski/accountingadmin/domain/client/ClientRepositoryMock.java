fee9bb2f5e61daccbbe47af04bff94456c70aac5
Ivor <tkivor@gmail.com>
2024-06-24 18:05:08 +0200

    Switched getRequestedOrLast method to getRequestedOrGenerateAndSave

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientRepositoryMock.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientRepositoryMock.java
@@ -70,7 +70,7 @@ class ClientRepositoryMock implements ClientRepository {
         return ClientDao.builder()
                 .id(Id.ofValue(size()))
                 .name(FullNameGenerator.generate())
-                .addressId(addressFacade.getRequestedOrLast(Id.ofValue(size())).getId())
+                .addressId(addressFacade.getRequestedOrGenerateAndSave(Id.ofValue(size())).getId())
                 .build();
     }
 }

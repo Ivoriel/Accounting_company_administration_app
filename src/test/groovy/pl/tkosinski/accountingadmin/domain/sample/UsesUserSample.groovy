9607d4d7af06adcc773a2425b8ffdc9201afac8b
Ivor <tkivor@gmail.com>
2024-07-01 17:12:00 +0200

    provided userDto sample

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesUserSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesUserSample.groovy
@@ -3,6 +3,7 @@ package pl.tkosinski.accountingadmin.domain.sample
 import pl.tkosinski.accountingadmin.common.model.FullName
 import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.user.UserDao
+import pl.tkosinski.accountingadmin.domain.user.dto.UserDto
 
 import static pl.tkosinski.accountingadmin.common.model.Role.EMPLOYEE
 
@@ -10,6 +11,13 @@ trait UsesUserSample {
 
     UserDao.UserDaoBuilder userDaoSample(def args = null) {
         return UserDao.builder()
+                .id(Id.ofValue(args?.id ?: 16))
+                .role(args?.role ?: EMPLOYEE)
+                .name(FullName.ofValue("Teodor", "Nowak"))
+    }
+
+    UserDto.UserDtoBuilder userDtoSample(def args = null) {
+        return UserDto.builder()
                 .id(Id.ofValue(args?.id ?: 16))
                 .role(args?.role ?: EMPLOYEE)
                 .name(FullName.ofValue("Teodor", "Nowak"))

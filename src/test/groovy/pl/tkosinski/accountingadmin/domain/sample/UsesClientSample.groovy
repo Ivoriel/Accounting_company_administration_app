17e1648d8cdd903b1167893a1ae678b73caac03e
Ivor <tkivor@gmail.com>
2024-07-02 01:20:23 +0200

    provided clientDto sample

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesClientSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesClientSample.groovy
@@ -3,6 +3,7 @@ package pl.tkosinski.accountingadmin.domain.sample
 import pl.tkosinski.accountingadmin.common.model.FullName
 import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.client.ClientDao
+import pl.tkosinski.accountingadmin.domain.client.dto.ClientDto
 
 trait UsesClientSample {
 
@@ -13,4 +14,10 @@ trait UsesClientSample {
                 .addressId(Id.ofValue(165))
     }
 
+    ClientDto.ClientDtoBuilder clientDtoSample(def args = null) {
+        ClientDto.builder()
+                .id(Id.ofValue(args?.id ?: 16))
+                .name(FullName.ofValue("Teodor", "Nowak"))
+                .addressId(Id.ofValue(165))
+    }
 }

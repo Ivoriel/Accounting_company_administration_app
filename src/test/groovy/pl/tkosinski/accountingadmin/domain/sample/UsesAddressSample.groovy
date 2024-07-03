f16351d89fcb7238859d2cbd6cb6404144e3d498
Ivor <tkivor@gmail.com>
2024-07-03 11:35:24 +0200

    provided addressDto sample

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesAddressSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesAddressSample.groovy
@@ -2,6 +2,7 @@ package pl.tkosinski.accountingadmin.domain.sample
 
 import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.address.AddressDao
+import pl.tkosinski.accountingadmin.domain.address.dto.AddressDto
 
 import java.util.concurrent.ThreadLocalRandom
 
@@ -9,6 +10,18 @@ trait UsesAddressSample {
 
     AddressDao.AddressDaoBuilder addressDaoSample(def args = null) {
         AddressDao.builder()
+                .id(Id.ofValue(args?.id ?: 125))
+                .country("Polska")
+                .municipality("Toruń")
+                .region("kujawsko-pomorskie")
+                .zipCode("87-100")
+                .street("Jasna")
+                .buildingNumber("1")
+                .additionalIdentifier("2p")
+    }
+
+    AddressDto.AddressDtoBuilder addressDtoSample(def args = null) {
+        AddressDto.builder()
                 .id(Id.ofValue(args?.id ?: 125))
                 .country("Polska")
                 .municipality("Toruń")

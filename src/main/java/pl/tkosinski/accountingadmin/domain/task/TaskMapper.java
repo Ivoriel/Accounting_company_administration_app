7f10cb6f499302b6d2f2b120afe3437c60936ff7
Ivor <tkivor@gmail.com>
2024-07-16 00:56:57 +0200

    Switched TaskMapper to package private

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskMapper.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskMapper.java
@@ -2,7 +2,7 @@ package pl.tkosinski.accountingadmin.domain.task;
 
 import pl.tkosinski.accountingadmin.domain.task.dto.TaskDto;
 
-public class TaskMapper {
+class TaskMapper {
 
     public static TaskDto toDto(TaskDao dao) {
         return TaskDto.builder()

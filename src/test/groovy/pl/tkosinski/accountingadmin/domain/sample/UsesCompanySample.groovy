f7f34d37a688ae1b795323efa4ad76962507608f
Ivor <tkivor@gmail.com>
2024-08-12 00:09:05 +0200

    Added companyDtoSample

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesCompanySample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesCompanySample.groovy
@@ -3,6 +3,7 @@ package pl.tkosinski.accountingadmin.domain.sample
 import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.common.model.InstitutionName
 import pl.tkosinski.accountingadmin.domain.company.CompanyDao
+import pl.tkosinski.accountingadmin.domain.company.dto.CompanyDto
 
 trait UsesCompanySample {
 
@@ -14,4 +15,11 @@ trait UsesCompanySample {
                 .clientId(Id.ofValue(818))
     }
 
+    CompanyDto.CompanyDtoBuilder companyDtoSample(def args = null) {
+        CompanyDto.builder()
+                .id(Id.ofValue(args?.id ?: 16))
+                .name(InstitutionName.ofValue("Test company name"))
+                .addressId(Id.ofValue(919))
+                .clientId(Id.ofValue(818))
+    }
 }

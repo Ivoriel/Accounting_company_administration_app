5a87aad9648b91a8abfbcff30887ff52789e3bf9
Ivor <tkivor@gmail.com>
2024-09-12 18:20:10 +0200

    Converted UserDto to record

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserDto.java
@@ -1,16 +1,9 @@
 package pl.tkosinski.accountingadmin.domain.user.dto;
 
-import lombok.Builder;
-import lombok.Value;
 import pl.tkosinski.accountingadmin.common.model.FullName;
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.Role;
 
-@Value
-@Builder
-public class UserDto {
+public record UserDto(Id id, Role role, FullName name) {
 
-    Id id;
-    Role role;
-    FullName name;
 }

178b76cab3a9bc9b1a02aca55d1db34b5d6e8a89
Ivor <tkivor@gmail.com>
2024-09-16 18:33:59 +0200

    Switched class to record

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/dto/CompanyDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/dto/CompanyDto.java
@@ -1,17 +1,6 @@
 package pl.tkosinski.accountingadmin.domain.company.dto;
 
-import lombok.Builder;
-import lombok.Value;
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.InstitutionName;
 
-@Value
-@Builder
-public class CompanyDto {
-
-    Id id;
-    InstitutionName name;
-    Id clientId;
-    Id addressId;
-
-}
+public record CompanyDto(Id id, InstitutionName name, Id clientId, Id addressId) {}

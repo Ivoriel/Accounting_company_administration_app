ad16e3c709cc2a7b495b1695d0ed74a28f29667e
Ivor <tkivor@gmail.com>
2024-09-17 19:30:42 +0200

    Switched class to record

--- src/main/java/pl/tkosinski/accountingadmin/common/model/Id.java
+++ src/main/java/pl/tkosinski/accountingadmin/common/model/Id.java
@@ -1,33 +1,8 @@
 package pl.tkosinski.accountingadmin.common.model;
 
-import java.util.Objects;
-
-public class Id {
-    
-    private final long value;
-
-    private Id(long value) {
-        this.value = value;
-    }
+public record Id(long value) {
 
     public static Id ofValue(long value) {
         return new Id(value);
-    }
-
-    public long getValue() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Id id = (Id) o;
-        return value == id.value;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
     }
 }

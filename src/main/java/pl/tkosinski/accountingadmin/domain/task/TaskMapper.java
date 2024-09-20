eb75fbba7e9c1895bf75c5837d0c5cf9511b582d
Ivor <tkivor@gmail.com>
2024-09-20 09:46:11 +0200

    Switch from builder to constructor

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskMapper.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskMapper.java
@@ -5,14 +5,7 @@ import pl.tkosinski.accountingadmin.domain.task.dto.TaskDto;
 class TaskMapper {
 
     public static TaskDto toDto(TaskDao dao) {
-        return TaskDto.builder()
-                .id(dao.getId())
-                .performerId(dao.getPerformerId())
-                .clientCompanyId(dao.getClientCompanyId())
-                .start(dao.getStart())
-                .end(dao.getEnd())
-                .title(dao.getTitle())
-                .comment(dao.getComment())
-                .build();
+        return new TaskDto(dao.getId(), dao.getPerformerId(), dao.getClientCompanyId(), dao.getStart(), dao.getEnd(),
+                dao.getTitle(), dao.getComment());
     }
 }

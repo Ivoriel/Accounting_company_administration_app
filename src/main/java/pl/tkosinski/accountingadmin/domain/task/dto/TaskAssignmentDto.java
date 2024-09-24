c1fe7ab15e89a0bb701f969a58a5e13f22a28fad
Ivor <tkivor@gmail.com>
2024-09-24 19:11:11 +0200

    Switched class to record

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskAssignmentDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskAssignmentDto.java
@@ -2,21 +2,4 @@ package pl.tkosinski.accountingadmin.domain.task.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
 
-public class TaskAssignmentDto {
-
-    private final Id taskId;
-    private final Id performerId;
-
-    public TaskAssignmentDto(Id taskId, Id performerId) {
-        this.taskId = taskId;
-        this.performerId = performerId;
-    }
-
-    public Id getTaskId() {
-        return taskId;
-    }
-
-    public Id getPerformerId() {
-        return performerId;
-    }
-}
+public record TaskAssignmentDto(Id taskId, Id performerId) {}

1866fe7fc94702f736c313549234b28166a97d16
Ivor <tkivor@gmail.com>
2024-09-26 00:20:24 +0200

    Added private constructor

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientMapper.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientMapper.java
@@ -2,7 +2,13 @@ package pl.tkosinski.accountingadmin.domain.client;
 
 import pl.tkosinski.accountingadmin.domain.client.dto.ClientDto;
 
+import static pl.tkosinski.accountingadmin.common.exception.code.Utility.UTILITY_CLASS;
+
 class ClientMapper {
+
+    private ClientMapper() {
+        throw new IllegalStateException(UTILITY_CLASS.message);
+    }
 
     public static ClientDto toDto(ClientDao dao) {
         return ClientDto.builder()

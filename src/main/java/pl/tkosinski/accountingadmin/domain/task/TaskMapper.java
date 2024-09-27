bbfa8ac283d6e5d72485366b72bdc314a3ead02d
Ivor <tkivor@gmail.com>
2024-09-28 01:00:54 +0200

    Added private constructor

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskMapper.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/TaskMapper.java
@@ -2,7 +2,13 @@ package pl.tkosinski.accountingadmin.domain.task;
 
 import pl.tkosinski.accountingadmin.domain.task.dto.TaskDto;
 
+import static pl.tkosinski.accountingadmin.common.exception.code.Utility.UTILITY_CLASS;
+
 class TaskMapper {
+
+    private TaskMapper() {
+        throw new IllegalStateException(UTILITY_CLASS.message);
+    }
 
     public static TaskDto toDto(TaskDao dao) {
         return new TaskDto(dao.getId(), dao.getPerformerId(), dao.getClientCompanyId(), dao.getStart(), dao.getEnd(),

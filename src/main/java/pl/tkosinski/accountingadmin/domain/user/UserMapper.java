5eee1d9120f403d9f047fc30cf95cce3a7627b19
Ivor <tkivor@gmail.com>
2024-09-29 11:27:18 +0200

    Added private constructor

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/UserMapper.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/UserMapper.java
@@ -2,7 +2,13 @@ package pl.tkosinski.accountingadmin.domain.user;
 
 import pl.tkosinski.accountingadmin.domain.user.dto.UserDto;
 
+import static pl.tkosinski.accountingadmin.common.exception.code.Utility.UTILITY_CLASS;
+
 class UserMapper {
+
+    private UserMapper() {
+        throw new IllegalStateException(UTILITY_CLASS.message);
+    }
 
     public static UserDto toDto(UserDao dao) {
         return new UserDto(dao.getId(), dao.getRole(), dao.getName());

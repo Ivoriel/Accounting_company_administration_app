ac50d4076ddf8d3891ad05bcf926ea1e445eb83b
Ivor <tkivor@gmail.com>
2024-10-11 17:44:35 +0200

    Switched to utilize static method instead of instantiating the mapper

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/client/ClientMapperTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/client/ClientMapperTest.groovy
@@ -6,14 +6,12 @@ import spock.lang.Specification
 
 class ClientMapperTest extends Specification implements UsesClientSample {
 
-    ClientMapper mapper = new ClientMapper()
-
     def "should map client entity to dto"() {
         given:
         def dao = clientDaoSample().build()
 
         when:
-        def dto = mapper.toDto(dao)
+        def dto = ClientMapper.toDto(dao)
 
         then:
         dto.id == dao.id

a4e96de3c96a49e04d3c73587a4838cc99e5b5e9
Ivor <tkivor@gmail.com>
2024-10-27 08:33:36 +0100

    BaseController added

---
+++ src/main/java/pl/tkosinski/accountingadmin/api/BaseController.java
@@ -0,0 +1,30 @@
+package pl.tkosinski.accountingadmin.api;
+
+import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
+
+public class BaseController {
+
+    void validateAdmin(UserIdDto userId) {
+        if (!userId.isAdmin()) {
+            throw new UnsupportedOperationException("Admin privileges required for this operation");
+        }
+    }
+
+    void validateEmployee(UserIdDto userId) {
+        if (!userId.isEmployee()) {
+            throw new UnsupportedOperationException("Employee privileges required for this operation");
+        }
+    }
+
+    void validateAdminOrEmployee(UserIdDto userId) {
+        if (!userId.isEmployeeOrAdmin()) {
+            throw new UnsupportedOperationException("Admin or Employee privileges required for this operation");
+        }
+    }
+
+    void validateClient(UserIdDto userId) {
+        if (!userId.isClient()) {
+            throw new UnsupportedOperationException("Client privileges required for this operation");
+        }
+    }
+}

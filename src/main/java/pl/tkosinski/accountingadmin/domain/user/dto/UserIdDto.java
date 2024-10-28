d3542b2264b3fb1c1e2ec50f98a9ebb48008f1b1
Ivor <tkivor@gmail.com>
2024-10-28 17:25:53 +0100

    UserIdDto object added

---
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserIdDto.java
@@ -0,0 +1,37 @@
+package pl.tkosinski.accountingadmin.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import pl.tkosinski.accountingadmin.common.model.Role;
+
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+import static pl.tkosinski.accountingadmin.common.model.Role.ADMIN;
+import static pl.tkosinski.accountingadmin.common.model.Role.CLIENT;
+import static pl.tkosinski.accountingadmin.common.model.Role.EMPLOYEE;
+
+@AllArgsConstructor(access = PRIVATE)
+@NoArgsConstructor(access = PRIVATE, force = true)
+public class UserIdDto {
+
+    private static final List<Role> INTERNAL_ROLES = List.of(ADMIN, EMPLOYEE);
+
+    private final Role role;
+
+    public boolean isAdmin() {
+        return ADMIN == role;
+    }
+
+    public boolean isEmployee() {
+        return EMPLOYEE == role;
+    }
+
+    public boolean isEmployeeOrAdmin() {
+        return INTERNAL_ROLES.contains(role);
+    }
+
+    public boolean isClient() {
+        return CLIENT == role;
+    }
+}

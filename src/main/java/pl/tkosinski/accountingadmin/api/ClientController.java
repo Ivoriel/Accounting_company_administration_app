696ce254984a98ee5a93dc3f4af156b9aafdb02d
Ivor <tkivor@gmail.com>
2024-11-07 00:01:29 +0100

    Added validation to ep

--- src/main/java/pl/tkosinski/accountingadmin/api/ClientController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/ClientController.java
@@ -1,7 +1,14 @@
 package pl.tkosinski.accountingadmin.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.tkosinski.accountingadmin.common.dto.IdRequest;
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.domain.client.ClientFacade;
 import pl.tkosinski.accountingadmin.domain.client.dto.ClientDto;
@@ -9,7 +16,7 @@ import pl.tkosinski.accountingadmin.domain.client.dto.ClientDto;
 @RestController
 @RequestMapping("/client")
 @RequiredArgsConstructor
-class ClientController {
+class ClientController extends BaseController {
 
     private final ClientFacade facade;
 
@@ -23,9 +30,11 @@ class ClientController {
         return facade.get(id);
     }
 
-    @DeleteMapping("/{id}/delete")
-    public void delete(@PathVariable Id id) {
-        facade.delete(id);
+    @DeleteMapping("/delete")
+    public void delete(@RequestBody IdRequest request) {
+        validateAdminOrEmployee(request.getUserId());
+
+        facade.delete(request.getId());
     }
 
     @GetMapping("/generate")

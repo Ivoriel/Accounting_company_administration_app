dda93fa44b7d832f46f1c100ec2b0ca5ed3d2b6c
Ivor <tkivor@gmail.com>
2024-11-14 18:11:50 +0100

    Reorganized imports

--- src/main/java/pl/tkosinski/accountingadmin/api/UserController.java
+++ src/main/java/pl/tkosinski/accountingadmin/api/UserController.java
@@ -1,7 +1,14 @@
 package pl.tkosinski.accountingadmin.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.tkosinski.accountingadmin.common.dto.IdRequest;
 import pl.tkosinski.accountingadmin.common.model.FullName;
 import pl.tkosinski.accountingadmin.common.model.Id;

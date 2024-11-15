d4c5bc3a68c13d31b95b1377f54dd36ddaacab1f
Ivor <tkivor@gmail.com>
2024-11-15 17:20:04 +0100

    Added UserIdDto to request object

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskAssignmentDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskAssignmentDto.java
@@ -1,5 +1,6 @@
 package pl.tkosinski.accountingadmin.domain.task.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
 
-public record TaskAssignmentDto(Id taskId, Id performerId) {}
+public record TaskAssignmentDto(UserIdDto userDto, Id taskId, Id performerId) {}

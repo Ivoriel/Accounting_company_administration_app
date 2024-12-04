3e521ed9c4fe18415c0926d00c43d268dcbdd731
Ivor <tkivor@gmail.com>
2024-12-04 17:14:29 +0100

    Added UserIdDto to TaskDto

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskDto.java
@@ -2,13 +2,16 @@ package pl.tkosinski.accountingadmin.domain.task.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.Text;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
 
 import java.time.LocalDateTime;
 
-public record TaskDto(Id id,
-                      Id performerId,
-                      Id clientCompanyId, LocalDateTime start,
-                      LocalDateTime end, Text title,
-                      Text comment) {
+public record TaskDto(
+        UserIdDto userId,
+        Id id,
+        Id performerId,
+        Id clientCompanyId, LocalDateTime start,
+        LocalDateTime end, Text title,
+        Text comment) {
 
 }

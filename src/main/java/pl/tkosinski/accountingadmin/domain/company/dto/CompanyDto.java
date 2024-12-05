551ec253bf98aa6cc377ae910866d468e715670a
Ivor <tkivor@gmail.com>
2024-12-05 19:03:35 +0100

    Added UserIdDto to CompanyDto

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/dto/CompanyDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/dto/CompanyDto.java
@@ -2,5 +2,6 @@ package pl.tkosinski.accountingadmin.domain.company.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.InstitutionName;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
 
-public record CompanyDto(Id id, InstitutionName name, Id clientId, Id addressId) {}
+public record CompanyDto(UserIdDto userId, Id id, InstitutionName name, Id clientId, Id addressId) {}

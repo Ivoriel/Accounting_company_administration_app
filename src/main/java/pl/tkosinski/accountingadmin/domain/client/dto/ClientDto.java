145877db9f36273dace4703414383231baf3e078
Ivor <tkivor@gmail.com>
2024-12-06 18:10:43 +0100

    Turned class into record and added UserIdDto to ClientDto

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/dto/ClientDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/dto/ClientDto.java
@@ -1,22 +1,7 @@
 package pl.tkosinski.accountingadmin.domain.client.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.Value;
 import pl.tkosinski.accountingadmin.common.model.FullName;
 import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
 
-import static lombok.AccessLevel.PRIVATE;
-
-@Value
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = PRIVATE, force = true)
-public class ClientDto {
-
-    Id id;
-    FullName name;
-    Id addressId;
-
-}
+public record ClientDto (UserIdDto userId, Id id, FullName name, Id addressId) {}

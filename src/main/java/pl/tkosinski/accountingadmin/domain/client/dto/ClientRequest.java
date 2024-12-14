04ecd31ead14b43ac10b90a08726e97976968041
Ivor <tkivor@gmail.com>
2024-12-14 10:39:35 +0100

    Added ClientRequest

---
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/dto/ClientRequest.java
@@ -0,0 +1,7 @@
+package pl.tkosinski.accountingadmin.domain.client.dto;
+
+import pl.tkosinski.accountingadmin.common.model.FullName;
+import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
+
+public record ClientRequest(UserIdDto userId, Id id, FullName name, Id addressId) {}

279fcdd1d8d5be643389418ca5641f3314857cc9
Ivor <tkivor@gmail.com>
2024-12-22 07:10:50 +0100

    Import clean-up

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskDto.java
@@ -2,7 +2,6 @@ package pl.tkosinski.accountingadmin.domain.task.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.Text;
-import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
 
 import java.time.LocalDateTime;
 

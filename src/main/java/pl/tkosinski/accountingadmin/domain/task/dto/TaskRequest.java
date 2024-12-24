e5776abf6d0b980c7fa34488ef92731b53e7932a
Ivor <tkivor@gmail.com>
2024-12-24 11:51:14 +0100

    Created TaskRequest

---
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskRequest.java
@@ -0,0 +1,17 @@
+package pl.tkosinski.accountingadmin.domain.task.dto;
+
+import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.common.model.Text;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
+
+import java.time.LocalDateTime;
+
+public record TaskRequest(
+        UserIdDto userId,
+        Id id,
+        Id performerId,
+        Id clientCompanyId, LocalDateTime start,
+        LocalDateTime end, Text title,
+        Text comment) {
+
+}

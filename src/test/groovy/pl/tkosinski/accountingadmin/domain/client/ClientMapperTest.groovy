9bffb5fb8c2fcdb355efddcdb6b25c7f2687f4ee
Ivor <tkivor@gmail.com>
2025-01-27 20:59:56 +0100

    Added test to ensure ClientMapper can't be instantiated.

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/client/ClientMapperTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/client/ClientMapperTest.groovy
@@ -1,10 +1,17 @@
 package pl.tkosinski.accountingadmin.domain.client
 
-
 import pl.tkosinski.accountingadmin.domain.sample.UsesClientSample
 import spock.lang.Specification
 
 class ClientMapperTest extends Specification implements UsesClientSample {
+
+    def "should not allow creating TaskMapper object"() {
+        when:
+        new ClientMapper()
+
+        then:
+        thrown(IllegalStateException.class)
+    }
 
     def "should map client entity to dto"() {
         given:

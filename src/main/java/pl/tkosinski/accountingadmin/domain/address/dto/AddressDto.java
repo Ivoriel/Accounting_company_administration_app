86d81bb54747d2c74e1320c981a610316f43e401
Ivor <tkivor@gmail.com>
2025-03-05 00:05:13 +0100

    Restored builder to AddressDto

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/dto/AddressDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/dto/AddressDto.java
@@ -1,6 +1,8 @@
 package pl.tkosinski.accountingadmin.domain.address.dto;
 
+import lombok.Builder;
 import pl.tkosinski.accountingadmin.common.model.Id;
 
+@Builder
 public record AddressDto(Id id, String country, String municipality, String region, String zipCode, String street,
                          String buildingNumber, String additionalIdentifier) {}

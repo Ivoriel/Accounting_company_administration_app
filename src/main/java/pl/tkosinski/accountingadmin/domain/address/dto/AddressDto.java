47c604ba23321a34261ae3a7b42d4a52a1668e8e
Ivor <tkivor@gmail.com>
2025-03-04 18:41:20 +0100

    Turned AddressDto to a record

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/dto/AddressDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/dto/AddressDto.java
@@ -1,25 +1,6 @@
 package pl.tkosinski.accountingadmin.domain.address.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.Value;
 import pl.tkosinski.accountingadmin.common.model.Id;
 
-import static lombok.AccessLevel.PRIVATE;
-
-@Value
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = PRIVATE, force = true)
-public class AddressDto {
-
-    Id id;
-    String country;
-    String municipality;
-    String region;
-    String zipCode;
-    String street;
-    String buildingNumber;
-    String additionalIdentifier;
-}
+public record AddressDto(Id id, String country, String municipality, String region, String zipCode, String street,
+                         String buildingNumber, String additionalIdentifier) {}

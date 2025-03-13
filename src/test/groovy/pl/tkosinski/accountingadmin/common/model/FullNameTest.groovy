1114c25ba2087cf5ee1e2f5752c7a616f230eda2
Ivor <tkivor@gmail.com>
2025-03-14 00:23:07 +0100

    Added validation method test

--- src/test/groovy/pl/tkosinski/accountingadmin/common/model/FullNameTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/common/model/FullNameTest.groovy
@@ -4,6 +4,16 @@ import spock.lang.Specification
 
 class FullNameTest extends Specification {
 
+    def "should not throw exception when given and last name parameters are null"() {
+        given:
+        def name = FullName.ofValue("Jan", "Kowalski")
+
+        when:
+        name.validate()
+
+        then:
+        noExceptionThrown()
+    }
 
     def "should throw exception when given and last name parameters are null"() {
         given:
@@ -14,8 +24,5 @@ class FullNameTest extends Specification {
 
         then:
         thrown(UnsupportedOperationException)
-    }
-
-    def "Validate"() {
     }
 }

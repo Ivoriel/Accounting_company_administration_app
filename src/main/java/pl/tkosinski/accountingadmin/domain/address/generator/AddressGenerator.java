75aee5f9caaa58d55f8c1173a79aa528c7a2a257
Ivor <tkivor@gmail.com>
2025-04-19 13:14:25 +0200

    AddressGenerator added

---
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/generator/AddressGenerator.java
@@ -0,0 +1,97 @@
+package pl.tkosinski.accountingadmin.domain.address.generator;
+
+import lombok.AllArgsConstructor;
+import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.domain.address.AddressFacade;
+import pl.tkosinski.accountingadmin.domain.address.dto.AddressDto;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@AllArgsConstructor
+class AddressGenerator {
+
+    private static final String COUNTRY = "Polska";
+
+    private final AddressFacade facade;
+
+    public AddressDto generate() {
+        return generateAddress();
+    }
+
+    public AddressDto generateAndSave() {
+        return facade.save(generate());
+    }
+
+    private AddressDto generateAddress() {
+        return new AddressDto(Id.generate(), generateCountry(), generateMunicipality(), generateRegion(),
+                generateZipCode(), generateStreet(), generateBuildingNumber(), generateAdditionalIdentifier());
+    }
+
+    private String generateCountry() {
+        return COUNTRY;
+    }
+
+    private String generateMunicipality() {
+        String[] municipalities = {"Toruń", "Płock", "Lubicz", "Warszawa", "Sosnowiec", "Ostrołęka", "Żywiec", "Modlin",
+                                    "Inowrocław", "Głogowo"};
+        return municipalities[generateRandomInt(0, municipalities.length - 1)];
+    }
+
+    private String generateRegion() {
+        String[] regions = {"Kujawsko-Pomorskie", "Mazowieckie", "Śląskie"};
+        return regions[generateRandomInt(0, regions.length - 1)];
+    }
+
+    private String generateZipCode() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            if (i == 2) {
+                sb.append("-");
+            } else {
+                sb.append(generateRandomInt(0, 9));
+            }
+        }
+        return sb.toString();
+    }
+
+    private String generateStreet() {
+        String[] streets = {"Prosta", "Szeroka", "Długa", "Mickiewicza", "Slowackiego", "Solidarności", "Grodzka",
+                            "Bohaterów Mariupola", "Wiśniowa", "Kochanowskiego", "Leśna"};
+        return streets[generateRandomInt(0,9)];
+    }
+
+    private String generateBuildingNumber() {
+        StringBuilder sb = new StringBuilder();
+        int buildingNumber = generateRandomInt(1, 115);
+        sb.append(buildingNumber);
+        if (buildingNumber % 4 == 0 || buildingNumber % 7 == 0) {
+            sb.append("A");
+            return sb.toString();
+        }
+        if (buildingNumber % 9 == 0) {
+            sb.append("B");
+            return sb.toString();
+        }
+        return sb.toString();
+    }
+
+    private String generateAdditionalIdentifier() {
+        StringBuilder sb = new StringBuilder();
+        int additionalIdentifier = generateRandomInt(1, 156);
+        sb.append(additionalIdentifier);
+        if (additionalIdentifier % 7 == 0) {
+            sb.append("Piętro 1");
+            return sb.toString();
+        }
+        if (additionalIdentifier % 9 == 0) {
+            sb.append("Piętro 3");
+            return sb.toString();
+        }
+        return sb.toString();
+    }
+
+    private int generateRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max +1);
+    }
+
+}

796834dbf91a316f94f38a1191dc75d9ef194555
Ivor <tkivor@gmail.com>
2025-04-25 23:22:42 +0200

    Implemented AddressRecord interface

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/dto/AddressRequest.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/dto/AddressRequest.java
@@ -3,5 +3,13 @@ package pl.tkosinski.accountingadmin.domain.address.dto;
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
 
-public record AddressRequest (UserIdDto userId, Id id, String country, String municipality, String region,
-                              String zipCode, String street, String buildingNumber, String additionalIdentifier) {}
+public record AddressRequest (
+        UserIdDto userId,
+        Id id,
+        String country,
+        String municipality,
+        String region,
+        String zipCode,
+        String street,
+        String buildingNumber,
+        String additionalIdentifier) implements AddressRecord{}

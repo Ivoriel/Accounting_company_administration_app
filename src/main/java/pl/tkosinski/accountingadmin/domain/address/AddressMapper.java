6c83609186ac9591829c6961445a3df577f62a2f
Ivor <tkivor@gmail.com>
2025-04-27 21:21:59 +0200

    Added method to map records to dao

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressMapper.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressMapper.java
@@ -1,6 +1,7 @@
 package pl.tkosinski.accountingadmin.domain.address;
 
 import pl.tkosinski.accountingadmin.domain.address.dto.AddressDto;
+import pl.tkosinski.accountingadmin.domain.address.dto.AddressRecord;
 
 import static pl.tkosinski.accountingadmin.common.exception.code.Utility.UTILITY_CLASS;
 
@@ -20,6 +21,19 @@ class AddressMapper {
                 .street(addressDao.getStreet())
                 .buildingNumber(addressDao.getBuildingNumber())
                 .additionalIdentifier(addressDao.getAdditionalIdentifier())
+                .build();
+    }
+
+    public static AddressDao toDao(AddressRecord dto) {
+        return AddressDao.builder()
+                .id(dto.id())
+                .country(dto.country())
+                .municipality(dto.municipality())
+                .region(dto.region())
+                .zipCode(dto.zipCode())
+                .street(dto.street())
+                .buildingNumber(dto.buildingNumber())
+                .additionalIdentifier(dto.additionalIdentifier())
                 .build();
     }
 }

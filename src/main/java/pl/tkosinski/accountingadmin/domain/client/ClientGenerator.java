c4ef17a23e26b73c35343c001ff7649aa77c6397
Ivor <tkivor@gmail.com>
2025-04-30 00:20:38 +0200

    Added ClientGenerator method utilizing AddressFacade

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientGenerator.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientGenerator.java
@@ -2,9 +2,17 @@ package pl.tkosinski.accountingadmin.domain.client;
 
 import pl.tkosinski.accountingadmin.common.generator.FullNameGenerator;
 import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.domain.address.AddressFacade;
 import pl.tkosinski.accountingadmin.domain.client.dto.ClientDto;
 
 public class ClientGenerator {
+
+    public ClientDto generateClient(AddressFacade addressFacade) {
+        return new ClientDto(
+                Id.generate(),
+                FullNameGenerator.generate(),
+                addressFacade.generateAndSave().id());
+    }
 
     public ClientDto generateClient(Id addressId) {
         return new ClientDto(

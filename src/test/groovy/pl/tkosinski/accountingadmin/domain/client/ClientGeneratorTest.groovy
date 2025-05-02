224fc2802385a314fa69fc484880a584b7417d48
Ivor <tkivor@gmail.com>
2025-05-03 00:03:20 +0200

    Added test utilizing generate with address facade parameter

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/client/ClientGeneratorTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/client/ClientGeneratorTest.groovy
@@ -1,11 +1,12 @@
 package pl.tkosinski.accountingadmin.domain.client
 
 import pl.tkosinski.accountingadmin.common.model.Id
+import pl.tkosinski.accountingadmin.domain.address.AddressFacade
 import spock.lang.Specification
 
 class ClientGeneratorTest extends Specification {
 
-    def "should generate and return client"() {
+    def "should generate and return client given addressId"() {
         given:
         def generator = new ClientGenerator()
         def addressId = Id.generate()
@@ -18,6 +19,16 @@ class ClientGeneratorTest extends Specification {
         generatedDto.addressId() != null
     }
 
-    def TestGenerate() {
+    def "should generate and return client given addressFacade"() {
+        given:
+        def generator = new ClientGenerator()
+        def addressFacade = new AddressFacade()
+
+        when:
+        def generatedDto = generator.generate(addressFacade)
+
+        then:
+        generatedDto.name() != null
+        generatedDto.addressId() != null
     }
 }

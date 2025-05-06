7f34fa7535365fdf7aca42c5c79b3a588000798f
Ivor <tkivor@gmail.com>
2025-05-06 22:06:30 +0200

    Added access methods

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/dto/ClientRequest.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/dto/ClientRequest.java
@@ -4,4 +4,17 @@ import pl.tkosinski.accountingadmin.common.model.FullName;
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
 
-public record ClientRequest(UserIdDto userId, Id id, FullName name, Id addressId) {}
+public record ClientRequest(UserIdDto userId, ClientDto clientData) {
+
+    public Id id() {
+        return clientData.id();
+    }
+
+    public FullName name() {
+        return clientData.name();
+    }
+
+    public Id addressId() {
+        return clientData.addressId();
+    }
+}

a1d464f23e7e26b8f455ca817a910a233c8c079e
Ivor <tkivor@gmail.com>
2025-05-13 21:17:23 +0200

    Renamed dao to entity

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientEntity.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientEntity.java
@@ -9,25 +9,25 @@ import pl.tkosinski.accountingadmin.domain.client.dto.ClientRequest;
 
 @Builder
 @Getter
-class ClientDao implements BaseDao {
+class ClientEntity implements BaseDao {
 
     private final Id id;
     private FullName name;
     private Id addressId;
 
-    public ClientDao(Id id, FullName name, Id addressId) {
+    public ClientEntity(Id id, FullName name, Id addressId) {
         this.id = id;
         this.name = name;
         this.addressId = addressId;
     }
 
-    public ClientDao(Id id, ClientRequest request) {
+    public ClientEntity(Id id, ClientRequest request) {
         this.id = id;
         this.name = request.name();
         this.addressId = request.addressId();
     }
 
-    public ClientDao edit(ClientRequest request) {
+    public ClientEntity edit(ClientRequest request) {
         this.name = request.name();
         this.addressId = request.addressId();
         return this;

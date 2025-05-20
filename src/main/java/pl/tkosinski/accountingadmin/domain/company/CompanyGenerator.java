c0b3f2db074a6a8398cf0d3db9c3661ef9ab3afc
Ivor <tkivor@gmail.com>
2025-05-21 00:01:37 +0200

    Added company generator

---
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/CompanyGenerator.java
@@ -0,0 +1,39 @@
+package pl.tkosinski.accountingadmin.domain.company;
+
+import pl.tkosinski.accountingadmin.common.model.Id;
+import pl.tkosinski.accountingadmin.common.model.InstitutionName;
+import pl.tkosinski.accountingadmin.domain.address.AddressFacade;
+import pl.tkosinski.accountingadmin.domain.client.ClientFacade;
+import pl.tkosinski.accountingadmin.domain.company.dto.CompanyDto;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CompanyGenerator {
+
+    public CompanyDto generate(ClientFacade clientFacade, AddressFacade addressFacade) {
+        return new CompanyDto(
+                Id.generate(),
+                generateCompanyName(),
+                clientFacade.getRequestedOrGenerateAndSave(Id.generate()).id(),
+                addressFacade.getRequestedOrGenerateAndSave(Id.generate()).id());
+    }
+
+    public CompanyDto generate(Id clientId, Id addressId) {
+        return new CompanyDto(
+                Id.generate(),
+                generateCompanyName(),
+                clientId,
+                addressId);
+    }
+
+    private InstitutionName generateCompanyName() {
+        String[] names = {"Januszex Janusz Typowy", "PolExport sp. z o.o.", "BiznesEx s.c.", "PolBiznes SA",
+                "ExKosmos s.j.", "Mydło i Powidło Jan Nowak", "Warzywa i owoca Ewelina Bluszcz", "PHU Kliper s.c.",
+                "Story-Train Bill Smith", "Gilgamesh sp. z o.o.", "Enkidu SA", "HusariaPol Marian Kmieć"};
+        return InstitutionName.ofValue(names[generateRandomInt(names.length - 1)]);
+    }
+
+    private int generateRandomInt(int max) {
+        return ThreadLocalRandom.current().nextInt(0, max +1);
+    }
+}

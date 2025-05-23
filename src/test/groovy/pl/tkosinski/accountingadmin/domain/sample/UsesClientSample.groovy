00d1808cd3e28228c9b9356384afe38729da692e
Ivor <tkivor@gmail.com>
2025-05-24 00:49:26 +0200

    Changes to client samples

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesClientSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesClientSample.groovy
@@ -2,13 +2,13 @@ package pl.tkosinski.accountingadmin.domain.sample
 
 import pl.tkosinski.accountingadmin.common.model.FullName
 import pl.tkosinski.accountingadmin.common.model.Id
-import pl.tkosinski.accountingadmin.domain.client.ClientDao
+import pl.tkosinski.accountingadmin.domain.client.ClientEntity
 import pl.tkosinski.accountingadmin.domain.client.dto.ClientDto
 
 trait UsesClientSample {
 
-    ClientDao.ClientDaoBuilder clientDaoSample(def args = null) {
-        ClientDao.builder()
+    ClientEntity.ClientEntityBuilder clientDaoSample(def args = null) {
+        ClientEntity.builder()
                 .id(args?.id ?: Id.generate())
                 .name(FullName.ofValue("Teodor", "Nowak"))
                 .addressId(Id.generate())

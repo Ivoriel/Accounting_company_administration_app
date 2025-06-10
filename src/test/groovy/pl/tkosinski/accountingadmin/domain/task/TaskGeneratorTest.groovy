053109df4bd0ccb8c6dbfdc919b0b37364bdeaf7
Ivor <tkivor@gmail.com>
2025-06-10 19:47:21 +0200

    Test added

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/task/TaskGeneratorTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/task/TaskGeneratorTest.groovy
@@ -14,7 +14,17 @@ class TaskGeneratorTest extends Specification {
     def "Generate"() {
     }
 
-    def "TestGenerate"() {
+    def "should generate and return task when all user and company ids are provided"() {
+        when:
+        def generatedDao = generator.generate(Id.generate(), Id.generate())
+
+        then:
+        generatedDao.performerId != null
+        generatedDao.clientCompanyId != null
+        generatedDao.start != null
+        generatedDao.end != null
+        generatedDao.title != null
+        generatedDao.comment != null
     }
 
     def "should generate and return task when all parameters are provided"() {

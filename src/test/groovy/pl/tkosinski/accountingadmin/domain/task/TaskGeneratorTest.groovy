d2046905061abf01447d8e0f0364af83d097f9c6
Ivor <tkivor@gmail.com>
2025-06-11 00:30:33 +0200

    Test added

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/task/TaskGeneratorTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/task/TaskGeneratorTest.groovy
@@ -1,10 +1,14 @@
 package pl.tkosinski.accountingadmin.domain.task
 
 import pl.tkosinski.accountingadmin.common.model.Id
+import pl.tkosinski.accountingadmin.domain.company.CompanyFacade
+import pl.tkosinski.accountingadmin.domain.user.UserFacade
 import spock.lang.Specification
 
 class TaskGeneratorTest extends Specification {
 
+    private final UserFacade userFacade
+    private final CompanyFacade companyFacade
     TaskGenerator generator;
 
     def setup() {
@@ -14,7 +18,20 @@ class TaskGeneratorTest extends Specification {
     def "Generate"() {
     }
 
-    def "should generate and return task when all user and company ids are provided"() {
+    def "should generate and return task when user and company facades and with start and end params are provided"() {
+        when:
+        def generatedDao = generator.generate(userFacade, companyFacade, 40, 220)
+
+        then:
+        generatedDao.performerId != null
+        generatedDao.clientCompanyId != null
+        generatedDao.start != null
+        generatedDao.end != null
+        generatedDao.title != null
+        generatedDao.comment != null
+    }
+
+    def "should generate and return task when user and company ids are provided"() {
         when:
         def generatedDao = generator.generate(Id.generate(), Id.generate())
 

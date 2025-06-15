8f9af8f54e3ce657ec2a648f4fc48359a39df83d
Ivor <tkivor@gmail.com>
2025-06-16 01:22:21 +0200

    Switched dao to entity

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesUserSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesUserSample.groovy
@@ -2,15 +2,15 @@ package pl.tkosinski.accountingadmin.domain.sample
 
 import pl.tkosinski.accountingadmin.common.model.FullName
 import pl.tkosinski.accountingadmin.common.model.Id
-import pl.tkosinski.accountingadmin.domain.user.UserDao
+import pl.tkosinski.accountingadmin.domain.user.UserEntity
 import pl.tkosinski.accountingadmin.domain.user.dto.UserDto
 
 import static pl.tkosinski.accountingadmin.common.model.Role.EMPLOYEE
 
 trait UsesUserSample {
 
-    UserDao.UserDaoBuilder userDaoSample(def args = null) {
-        UserDao.builder()
+    UserEntity.UserEntityBuilder userDaoSample(def args = null) {
+        UserEntity.builder()
                 .id(args?.id ?: Id.generate())
                 .role(args?.role ?: EMPLOYEE)
                 .name(FullName.ofValue("Teodor", "Nowak"))

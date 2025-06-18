dacc786ae5ec815a672d5fc77653530e64981abb
Ivor <tkivor@gmail.com>
2025-06-19 01:29:45 +0200

    Switched to use single implementation

--- src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientGenerator.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/client/ClientGenerator.java
@@ -8,10 +8,7 @@ import pl.tkosinski.accountingadmin.domain.client.dto.ClientDto;
 public class ClientGenerator {
 
     public ClientDto generate(AddressFacade addressFacade) {
-        return new ClientDto(
-                Id.generate(),
-                FullNameGenerator.generate(),
-                addressFacade.generateAndSave().id());
+        return generate(addressFacade.generateAndSave().id());
     }
 
     public ClientDto generate(Id addressId) {

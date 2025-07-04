c78fbc3a5a530cd211792dd808131cbd5a3ffd96
Ivor <tkivor@gmail.com>
2025-07-05 00:04:28 +0200

    Renamed Dao to Entity

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesCompanySample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesCompanySample.groovy
@@ -2,13 +2,13 @@ package pl.tkosinski.accountingadmin.domain.sample
 
 import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.common.model.InstitutionName
-import pl.tkosinski.accountingadmin.domain.company.CompanyDao
+import pl.tkosinski.accountingadmin.domain.company.CompanyEntity
 import pl.tkosinski.accountingadmin.domain.company.dto.CompanyDto
 
 trait UsesCompanySample {
 
-    CompanyDao.CompanyDaoBuilder companyDaoSample(def args = null) {
-        CompanyDao.builder()
+    CompanyEntity.CompanyEntityBuilder companyEntitySample(def args = null) {
+        CompanyEntity.builder()
                 .id(args?.id ?: Id.generate())
                 .name(InstitutionName.ofValue("Test company name"))
                 .addressId(args?.addressId ?: Id.generate())

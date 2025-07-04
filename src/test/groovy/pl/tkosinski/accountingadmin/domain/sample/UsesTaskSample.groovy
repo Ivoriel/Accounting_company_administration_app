5b0484a55d6b6a46d7ea1999cb69fb514c99b061
Ivor <tkivor@gmail.com>
2025-07-04 17:15:26 +0200

    Renamed Dao to Entity

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesTaskSample.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/sample/UsesTaskSample.groovy
@@ -1,15 +1,15 @@
 package pl.tkosinski.accountingadmin.domain.sample
 
-import pl.tkosinski.accountingadmin.common.model.Text
 import pl.tkosinski.accountingadmin.common.model.Id
-import pl.tkosinski.accountingadmin.domain.task.TaskDao
+import pl.tkosinski.accountingadmin.common.model.Text
+import pl.tkosinski.accountingadmin.domain.task.TaskEntity
 
 import java.time.LocalDateTime
 
 trait UsesTaskSample {
 
-    TaskDao.TaskDaoBuilder taskDaoSample(def args = null) {
-        TaskDao.builder()
+    TaskEntity.TaskEntityBuilder taskEntitySample(def args = null) {
+        TaskEntity.builder()
                 .id(args?.id ?: Id.generate())
                 .performerId(args?.performerId ?: Id.generate())
                 .start(LocalDateTime.now().minusMinutes(60))

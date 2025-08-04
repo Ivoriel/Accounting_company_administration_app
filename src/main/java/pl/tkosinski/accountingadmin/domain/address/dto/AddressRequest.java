71a277ea5c31dad320ab969449124756c8bd10a8
Ivor <tkivor@gmail.com>
2025-08-05 00:58:31 +0200

    Class renamed for accuracy

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/dto/AddressRequest.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/dto/AddressRequest.java
@@ -1,10 +1,10 @@
 package pl.tkosinski.accountingadmin.domain.address.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
-import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserRoleDto;
 
 public record AddressRequest (
-        UserIdDto userId,
+        UserRoleDto userId,
         Id id,
         String country,
         String municipality,

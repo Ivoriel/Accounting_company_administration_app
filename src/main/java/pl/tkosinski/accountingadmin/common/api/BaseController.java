54627a1a0489e51177a6aa4976c36130cd5ea959
Ivor <tkivor@gmail.com>
2025-08-06 03:54:29 +0200

    Class renamed for accuracy

--- src/main/java/pl/tkosinski/accountingadmin/common/api/BaseController.java
+++ src/main/java/pl/tkosinski/accountingadmin/common/api/BaseController.java
@@ -1,34 +1,34 @@
 package pl.tkosinski.accountingadmin.common.api;
 
-import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserRoleDto;
 
 public class BaseController {
 
-    protected void validateAdmin(UserIdDto userId) {
+    protected void validateAdmin(UserRoleDto userId) {
         if (!userId.isAdmin()) {
             throw new UnsupportedOperationException("Admin privileges required for this operation");
         }
     }
 
-    protected void validateEmployee(UserIdDto userId) {
+    protected void validateEmployee(UserRoleDto userId) {
         if (!userId.isEmployee()) {
             throw new UnsupportedOperationException("Employee privileges required for this operation");
         }
     }
 
-    protected void validateAdminOrEmployee(UserIdDto userId) {
+    protected void validateAdminOrEmployee(UserRoleDto userId) {
         if (!userId.isEmployeeOrAdmin()) {
             throw new UnsupportedOperationException("Admin or Employee privileges required for this operation");
         }
     }
 
-    protected void validateClient(UserIdDto userId) {
+    protected void validateClient(UserRoleDto userId) {
         if (!userId.isClient()) {
             throw new UnsupportedOperationException("Client privileges required for this operation");
         }
     }
 
-    protected void validateAuthorized(UserIdDto userId) {
+    protected void validateAuthorized(UserRoleDto userId) {
         if (!userId.isAuthorized()) {
             throw new UnsupportedOperationException("Authorized user privileges required for this operation");
         }

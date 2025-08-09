f27abd79c02f09f1ed398c058d583a675c27e79f
Ivor <tkivor@gmail.com>
2025-08-10 01:02:48 +0200

    Class renamed for accuracy

--- src/main/java/pl/tkosinski/accountingadmin/domain/company/dto/CompanyRequest.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/company/dto/CompanyRequest.java
@@ -2,6 +2,6 @@ package pl.tkosinski.accountingadmin.domain.company.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.InstitutionName;
-import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserRoleDto;
 
-public record CompanyRequest(UserIdDto userId, Id id, InstitutionName name, Id clientId, Id addressId) {}
+public record CompanyRequest(UserRoleDto userId, Id id, InstitutionName name, Id clientId, Id addressId) {}

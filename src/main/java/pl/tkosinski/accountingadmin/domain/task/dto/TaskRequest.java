6a73b013260544812475925862fb2580a15efdf7
Ivor <tkivor@gmail.com>
2025-08-15 01:01:48 +0200

    Class renamed for accuracy

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskRequest.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskRequest.java
@@ -2,12 +2,12 @@ package pl.tkosinski.accountingadmin.domain.task.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.Text;
-import pl.tkosinski.accountingadmin.domain.user.dto.UserIdDto;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserRoleDto;
 
 import java.time.LocalDateTime;
 
 public record TaskRequest(
-        UserIdDto userId,
+        UserRoleDto userId,
         Id id,
         Id performerId,
         Id clientCompanyId, LocalDateTime start,

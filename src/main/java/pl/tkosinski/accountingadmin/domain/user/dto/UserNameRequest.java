dd2d2b361b8cc25666d3e6649b6f218a1d3a71e0
Ivor <tkivor@gmail.com>
2025-08-17 01:45:21 +0200

    Class renamed for accuracy

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserNameRequest.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserNameRequest.java
@@ -3,4 +3,4 @@ package pl.tkosinski.accountingadmin.domain.user.dto;
 import pl.tkosinski.accountingadmin.common.model.FullName;
 import pl.tkosinski.accountingadmin.common.model.Id;
 
-public record UserNameRequest(UserIdDto userId, Id id, FullName name) {}
+public record UserNameRequest(UserRoleDto userId, Id id, FullName name) {}

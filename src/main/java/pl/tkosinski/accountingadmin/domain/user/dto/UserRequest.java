755e543900634e7fae18e52a4f53357c96e4c12b
Ivor <tkivor@gmail.com>
2025-08-18 02:57:59 +0200

    Class renamed for accuracy

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserRequest.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/dto/UserRequest.java
@@ -4,4 +4,4 @@ import pl.tkosinski.accountingadmin.common.model.FullName;
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.Role;
 
-public record UserRequest(UserIdDto userId, Id id, Role role, FullName name) {}
+public record UserRequest(UserRoleDto userId, Id id, Role role, FullName name) {}

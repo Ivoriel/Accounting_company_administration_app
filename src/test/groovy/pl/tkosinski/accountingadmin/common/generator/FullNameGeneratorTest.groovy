719c36b578d5de6415b9449c98c89e79372770f9
Ivor <tkivor@gmail.com>
2025-09-04 19:34:51 +0200

    Added test to ensure FullNameGenerator object can not be initialized

--- src/test/groovy/pl/tkosinski/accountingadmin/common/generator/FullNameGeneratorTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/common/generator/FullNameGeneratorTest.groovy
@@ -1,8 +1,17 @@
 package pl.tkosinski.accountingadmin.common.generator
 
+import pl.tkosinski.accountingadmin.domain.address.AddressMapper
 import spock.lang.Specification
 
 class FullNameGeneratorTest extends Specification {
+
+    def "should not allow creating FullNameGenerator object"() {
+        when:
+        new FullNameGenerator()
+
+        then:
+        thrown(IllegalStateException.class)
+    }
 
     def "should generate FullName object with given and last name attributes"() {
         when:

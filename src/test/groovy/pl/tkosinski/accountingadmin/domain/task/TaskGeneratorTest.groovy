0c529c9ea39318c003b67961f3ef8f4bd184ece3
Ivor <tkivor@gmail.com>
2025-09-05 00:07:24 +0200

    Test fixed

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/task/TaskGeneratorTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/task/TaskGeneratorTest.groovy
@@ -1,14 +1,15 @@
 package pl.tkosinski.accountingadmin.domain.task
 
+
 import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.company.CompanyFacade
+import pl.tkosinski.accountingadmin.domain.sample.UsesCompanySample
+import pl.tkosinski.accountingadmin.domain.sample.UsesUserSample
 import pl.tkosinski.accountingadmin.domain.user.UserFacade
 import spock.lang.Specification
 
-class TaskGeneratorTest extends Specification {
+class TaskGeneratorTest extends Specification implements UsesUserSample, UsesCompanySample {
 
-    UserFacade userFacade
-    CompanyFacade companyFacade
     TaskGenerator generator
 
     def setup() {
@@ -16,6 +17,12 @@ class TaskGeneratorTest extends Specification {
     }
 
     def "should generate and return task when user and company facades are provided"() {
+        given:
+        UserFacade userFacade = Mock()
+        userFacade.generate() >> userDtoSample()
+        CompanyFacade companyFacade = Mock()
+        companyFacade.generate() >> companyDtoSample()
+
         when:
         def generatedDto = generator.generate(userFacade, companyFacade)
 

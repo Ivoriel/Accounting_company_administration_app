4010aa61cdc1c960a906ad2a6e8c22c51cc0ef9d
Ivor <tkivor@gmail.com>
2025-09-09 00:27:32 +0200

    Test fixed

--- src/test/groovy/pl/tkosinski/accountingadmin/domain/company/CompanyGeneratorTest.groovy
+++ src/test/groovy/pl/tkosinski/accountingadmin/domain/company/CompanyGeneratorTest.groovy
@@ -3,9 +3,11 @@ package pl.tkosinski.accountingadmin.domain.company
 import pl.tkosinski.accountingadmin.common.model.Id
 import pl.tkosinski.accountingadmin.domain.address.AddressFacade
 import pl.tkosinski.accountingadmin.domain.client.ClientFacade
+import pl.tkosinski.accountingadmin.domain.sample.UsesAddressSample
+import pl.tkosinski.accountingadmin.domain.sample.UsesClientSample
 import spock.lang.Specification
 
-class CompanyGeneratorTest extends Specification {
+class CompanyGeneratorTest extends Specification implements UsesClientSample, UsesAddressSample {
 
     def "should generate and return company given clientId and addressId"() {
         given:
@@ -25,8 +27,10 @@ class CompanyGeneratorTest extends Specification {
     def "should generate and return company given clientFacade and addressFacade"() {
         given:
         def generator = new CompanyGenerator()
-        def clientFacade = new ClientFacade()
-        def addressFacade = new AddressFacade()
+        ClientFacade clientFacade = Mock()
+        clientFacade.generateAndSave() >> clientDtoSample()
+        AddressFacade addressFacade = Mock()
+        addressFacade.generateAndSave() >> addressDtoSample()
 
         when:
         def generatedDto = generator.generate(clientFacade, addressFacade)

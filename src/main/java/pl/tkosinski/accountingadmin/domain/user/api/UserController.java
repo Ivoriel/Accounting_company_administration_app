48b1d6359ba22b88842709ba596b02150cf81a76
Ivor <tkivor@gmail.com>
2025-09-23 18:01:07 +0200

    Removed redundant address mapping

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/api/UserController.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/api/UserController.java
@@ -37,7 +37,7 @@ class UserController extends BaseController {
         facade.editName(request);
     }
 
-    @GetMapping("/{id}/get")
+    @GetMapping("/get")
     public UserDto get(@RequestBody IdRequest request) {
         validateAdminOrEmployee(request.getUserId());
 

d45c51bb970a1ddc63e8ffa95777b6471bddd408
Ivor <tkivor@gmail.com>
2025-10-04 00:54:57 +0200

    Removed redundant address mapping

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/api/UserController.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/api/UserController.java
@@ -58,7 +58,7 @@ class UserController extends BaseController {
         facade.switchRoleToEmployee(request.getId());
     }
 
-    @PutMapping("/switch-role-client")
+    @PutMapping("switch-role-client")
     public void switchRoleToClient(@RequestBody IdRequest request) {
         validateAdmin(request.getUserId());
 

72446b032739e6594d97a958cdd59572196cc8e6
Ivor <tkivor@gmail.com>
2025-11-09 00:04:06 +0100

    Removed redundant address mapping

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/api/TaskController.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/api/TaskController.java
@@ -22,7 +22,7 @@ class TaskController extends BaseController {
 
     private final TaskFacade facade;
 
-    @PostMapping("save")
+    @PostMapping()
     public void save(@RequestBody TaskRequest request) {
         validateAdminOrEmployee(request.userId());
 

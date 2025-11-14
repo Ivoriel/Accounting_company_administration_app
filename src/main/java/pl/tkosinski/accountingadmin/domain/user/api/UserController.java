ce735e128778c3b2d13ac17693ccf793c42bd3da
Ivor <tkivor@gmail.com>
2025-11-14 19:05:44 +0100

    import cleanup

--- src/main/java/pl/tkosinski/accountingadmin/domain/user/api/UserController.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/user/api/UserController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +11,9 @@ import pl.tkosinski.accountingadmin.common.api.BaseController;
 import pl.tkosinski.accountingadmin.common.dto.IdRequest;
 import pl.tkosinski.accountingadmin.domain.user.UserFacade;
 import pl.tkosinski.accountingadmin.domain.user.dto.UserDto;
-import pl.tkosinski.accountingadmin.domain.user.dto.UserRoleDto;
 import pl.tkosinski.accountingadmin.domain.user.dto.UserNameRequest;
 import pl.tkosinski.accountingadmin.domain.user.dto.UserRequest;
+import pl.tkosinski.accountingadmin.domain.user.dto.UserRoleDto;
 
 @RestController
 @RequestMapping("user")

c1a94ba00fd3c39fd5972c93a19388719955f7f5
Ivor <tkivor@gmail.com>
2025-11-30 00:23:24 +0100

    Created TimeRange object

---
+++ src/main/java/pl/tkosinski/accountingadmin/common/model/TimeRange.java
@@ -0,0 +1,36 @@
+package pl.tkosinski.accountingadmin.common.model;
+
+import java.time.LocalDateTime;
+
+public class TimeRange {
+
+    private LocalDateTime from;
+    private LocalDateTime to;
+
+    private TimeRange() {}
+
+    private TimeRange(LocalDateTime from, LocalDateTime to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public TimeRange ofValue(LocalDateTime from, LocalDateTime to) {
+        return new TimeRange(from, to);
+    }
+
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
+    public LocalDateTime getTo() {
+        return to;
+    }
+
+    public void setFrom(LocalDateTime from) {
+        this.from = from;
+    }
+
+    public void setTo(LocalDateTime to) {
+        this.to = to;
+    }
+}

3122ea072d90a1620f5f94bbafb040dd858f5c67
Ivor <tkivor@gmail.com>
2025-12-09 17:03:25 +0100

    Removed potentially unnecessary parts of address

--- src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressEntity.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/address/AddressEntity.java
@@ -13,43 +13,30 @@ class AddressEntity implements BaseDao {
     private final Id id;
     private String country;
     private String municipality;
-    private String region;
     private String zipCode;
-    private String street;
-    private String buildingNumber;
-    private String additionalIdentifier;
+    private String streetAndBuildingId;
 
-    public AddressEntity(Id id, String country, String municipality, String region, String zipCode, String street,
-                         String buildingNumber, String additionalIdentifier) {
+    public AddressEntity(Id id, String country, String municipality, String zipCode, String streetAndBuildingId) {
         this.id = id;
         this.country = country;
         this.municipality = municipality;
-        this.region = region;
         this.zipCode = zipCode;
-        this.street = street;
-        this.buildingNumber = buildingNumber;
-        this.additionalIdentifier = additionalIdentifier;
+        this.streetAndBuildingId = streetAndBuildingId;
     }
 
     public AddressEntity(Id id, AddressRecord request) {
         this.id = id;
         this.country = request.country();
         this.municipality = request.municipality();
-        this.region = request.region();
         this.zipCode = request.zipCode();
-        this.street = request.street();
-        this.buildingNumber = request.buildingNumber();
-        this.additionalIdentifier = request.additionalIdentifier();
+        this.streetAndBuildingId = request.streetAndBuildingId();
     }
 
     public AddressEntity edit(AddressRecord request) {
         this.country = request.country();
         this.municipality = request.municipality();
-        this.region = request.region();
         this.zipCode = request.zipCode();
-        this.street = request.street();
-        this.buildingNumber = request.buildingNumber();
-        this.additionalIdentifier = request.additionalIdentifier();
+        this.streetAndBuildingId = request.streetAndBuildingId();
         return this;
     }
 }

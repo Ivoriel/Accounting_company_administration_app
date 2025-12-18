1b9640f06e66919706036f0675febe6c3c67815f
Ivor <tkivor@gmail.com>
2025-12-18 01:05:03 +0100

    Switched to TimeRange object

--- src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskDto.java
+++ src/main/java/pl/tkosinski/accountingadmin/domain/task/dto/TaskDto.java
@@ -2,14 +2,14 @@ package pl.tkosinski.accountingadmin.domain.task.dto;
 
 import pl.tkosinski.accountingadmin.common.model.Id;
 import pl.tkosinski.accountingadmin.common.model.Text;
-
-import java.time.LocalDateTime;
+import pl.tkosinski.accountingadmin.common.model.TimeRange;
 
 public record TaskDto(
         Id id,
         Id performerId,
-        Id clientCompanyId, LocalDateTime start,
-        LocalDateTime end, Text title,
+        Id clientCompanyId,
+        TimeRange period,
+        Text title,
         Text comment) {
 
 }
